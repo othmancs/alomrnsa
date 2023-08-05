c1988d860969d1560a4655bd2a21e4db73f8336c
othmancs <61784613+othmancs@users.noreply.github.com>
2023-08-05 14:34:05 +0300

    Add files via upload
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- bi_product_kit/__manifest__.py
+++ bi_product_kit/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Product Kit Bundle',
-    'version': '16.0.0.1',
+    'version': '16.0.0.0',
     'category': 'Sales',
     'summary': 'Combine product pack product kit product bundle product pack item on product combo product on sale bundle product delivery bundle product pack kit combine product combine product variant bundle item pack sales bundle delivery pack bundle kit products kit',
     'description' :""" This odoo app helps user to create a kit product, use existing product as a kit, add sub products with quantity and price, Calculate price from kit sub products or enter fix price, sell kit product only and create and deliver kit sub product, Also stock for kit sub products will be managed accordingly. Also customer invoice will created for kit product. Kit with sub products and quantity will also printed on sales order and customer invoice.
