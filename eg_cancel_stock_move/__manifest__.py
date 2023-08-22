64e9a3aa34ea9409581b32e5cc572db0f5442710
othmancs <61784613+othmancs@users.noreply.github.com>
2023-08-22 10:18:57 +0300

    Add files via upload
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

---
+++ eg_cancel_stock_move/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Cancel Stock Moves',
+    'version': '16.0',
+    'category': 'Warehouse',
+    'summery': 'Simple to refuse or cancel stock moves.',
+    'author': 'INKERP',
+    'website': "https://www.INKERP.com",
+    'depends': ['stock'],
+
+    'data': [
+            'views/stock_move_view.xml'
+    ],
+
+    'images': ['static/description/banner.png'],
+    'license': "OPL-1",
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
