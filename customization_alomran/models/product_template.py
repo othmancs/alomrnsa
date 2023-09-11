06bb7871ba5e7c2578159f820f264e6b841e7623
othmancs <61784613+othmancs@users.noreply.github.com>
2023-09-11 15:01:32 +0300

    Update product_template.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customization_alomran/models/product_template.py
+++ customization_alomran/models/product_template.py
@@ -31,20 +31,11 @@ class ProductTemplate(models.Model):
             img.save(temp, format="PNG")
             qr_code_image = base64.b64encode(temp.getvalue())
             rec.qr_code_img = qr_code_image
-
-    # @api.model
-    # def _name_search(self, name, args=None, operator='like', limit=100):
-    #     domain = args or []
-    #     if name:
-    #         pattern = re.compile('^{}'.format(re.escape(name)), re.IGNORECASE)
-    #         filtered_ids = [record.id for record in self.search(domain, limit=limit) if pattern.match(record.name)]
-    #         domain = [('id', 'in', filtered_ids)]
-    #     return super(ProductTemplate, self)._name_search(name, args=domain, operator=operator, limit=limit)
-    @api.model
-    def _name_search(self, name,default_code, args=None, operator='like', limit=100):
+   @api.model
+    def _name_search(self, name, args=None, operator='like', limit=100):
         domain = args or []
-        if default_code:
-            pattern = re.compile('^{}'.format(re.escape(default_code)), re.IGNORECASE)
-            filtered_ids = [record.id for record in self.search(domain, limit=limit) if pattern.match(record.default_code)]
+        if name:
+            pattern = re.compile('^{}'.format(re.escape(name)), re.IGNORECASE)
+            filtered_ids = [record.id for record in self.search(domain, limit=limit) if pattern.match(record.name)]
             domain = [('id', 'in', filtered_ids)]
-        return super(ProductTemplate, self)._name_search(default_code, args=domain, operator=operator, limit=limit)
+        return super(ProductTemplate, self)._name_search(name, args=domain, operator=operator, limit=limit)
