2906893781e88d6b9feec461c42ab3765ab0bd0c
othmancs <61784613+othmancs@users.noreply.github.com>
2023-09-12 10:27:32 +0300

    Delete customization_alomran directory
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customization_alomran/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import product_category
-from . import sale_order
-from . import product_template
-
--- customization_alomran/models/product_category.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class ProductCategory(models.Model):
-    _inherit = 'product.category'
-
-    stock_warehouse_id = fields.Many2one('stock.warehouse', string='Stock Warehouse')
