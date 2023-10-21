91e06f92ca62b0ad484cf5b6365cd62747bb8974
othmancs <61784613+othmancs@users.noreply.github.com>
2023-10-21 18:51:28 +0300

    Update product_template.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customization_alomran/models/product_template.py
+++ customization_alomran/models/product_template.py
@@ -41,11 +41,4 @@ class ProductTemplate(models.Model):
     #         print('filtered_ids',filtered_ids)
     #         domain = [('id', 'in', filtered_ids)]
     #     return super(ProductTemplate, self)._name_search( name=name,args=domain, operator=operator, limit=limit)
-    @api.model
-    def _name_search(self, name, args=None, operator='like', limit=100):
-        product_ids = []
-        if self._context.get("partner_id") and not self._context.get('product_id'):
-            partner_id = self.env["res.partner"].browse(self._context["partner_id"])
-            product_ids = self.with_context(product_id=True).search([('partner_id', '=', partner_id)])
-                return set(product_ids)
-        return super()._search(args, offset, limit, order, count, access_rights_uid)
+   
