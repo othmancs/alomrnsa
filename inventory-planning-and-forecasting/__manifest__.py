7535b3a4887211dd6e77eef9bbd95c26ddba0477
othmancs <61784613+othmancs@users.noreply.github.com>
2023-12-18 15:01:43 +0300

    Add files via upload
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

---
+++ inventory-planning-and-forecasting/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Inventory Planning & Forecasting',
+    'version': '1.0',
+    'summary': 'Skucast is inventory forecasting and planning tool which helps user to manage and plan inventory from one view. this is excel view made in react application. SKUcast, inventory management, demand forecasting, sales prediction, Odoo integration, Excel-like interface, AI sales analysis, marketplace support, security logs, data visualization, custom formulas, user actions tracking, multi-access, data upload, inventory planning, real-time analytics, warehouse management, product tracking, automated restocking, supply chain optimization, inventory levels, stock predictions, seasonal demand, lead time calculation, Odoo ERP, business intelligence, data consolidation, stock movement analysis, order management, purchase planning. ai forecasting, chatgpt analytics',
+    'author': 'TechFinna',
+    'website': 'https://www.techfinna.com',
+    'maintainer': 'Techfinna',
+    'category': 'Productivity',
+    'support': "info@techfinna.com",
+    'license': 'LGPL-3',
+    'price': 0,
+    'currency': 'USD',
+    'depends': ['base'],
+    'images': ['static/description/banner.gif'],
+   
+   
+    'application': True,
+    'installable': True,
+}
