8beb6b3aca8fcc72a5064c812b557a3fe67f8c8e
hossam <hossamwalid1997@gmail.com>
2024-01-09 11:47:38 +0200

    [FIX] sb_sales_invoices_credit_limit_final

--- sb_sales_invoices_credit_limit/model/account_move.py
+++ sb_sales_invoices_credit_limit/model/account_move.py
@@ -7,45 +7,46 @@ class AccountMove(models.Model):
 
     def action_post(self):
         for rec in self:
-            if rec.partner_id.sales_credit_limit > 0:
-                if not rec.line_ids.sale_line_ids.order_id:
-                    customer_due_amount = sum(self.env['account.move'].search([
-                        ('move_type', '=', 'out_invoice'),
-                        ('partner_id', '=', rec.partner_id.id),
-                        ('state', '=', 'posted')
-                    ]).mapped('amount_residual'))
+            if rec.move_type in ['out_invoice', 'in_invoice']:
+                if rec.partner_id.sales_credit_limit > 0:
+                    if not rec.line_ids.sale_line_ids.order_id:
+                        customer_due_amount = sum(self.env['account.move'].search([
+                            ('move_type', '=', 'out_invoice'),
+                            ('partner_id', '=', rec.partner_id.id),
+                            ('state', '=', 'posted')
+                        ]).mapped('amount_residual'))
 
-                    sale_without_invoice = self.env['sale.order'].search([
-                        ('partner_id', '=', rec.partner_id.id),
-                        ('invoice_ids', '=', False),
-                        ('state', 'in', ['sale', 'done'])
-                    ])
-                    if sale_without_invoice:
-                        order_amount = sum(sale_without_invoice.mapped('amount_total'))
-                        allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount - order_amount
-                        customer_due_amount += order_amount
-                    else:
-                        allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount
+                        sale_without_invoice = self.env['sale.order'].search([
+                            ('partner_id', '=', rec.partner_id.id),
+                            ('invoice_ids', '=', False),
+                            ('state', 'in', ['sale', 'done'])
+                        ])
+                        if sale_without_invoice:
+                            order_amount = sum(sale_without_invoice.mapped('amount_total'))
+                            allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount - order_amount
+                            customer_due_amount += order_amount
+                        else:
+                            allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount
 
-                    sale_with_invoice_draft = self.env['account.move'].search([
-                        ('move_type', '=', 'out_invoice'),
-                        ('partner_id', '=', rec.partner_id.id),
-                        ('state', '=', 'draft'),
-                        ('line_ids.sale_line_ids.order_id', '!=', False)
-                    ])
-                    if sale_with_invoice_draft:
-                        draft_order_amount = sum(sale_with_invoice_draft.mapped('amount_total'))
-                        allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount - draft_order_amount
-                        customer_due_amount += draft_order_amount
-                    else:
-                        allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount
+                        sale_with_invoice_draft = self.env['account.move'].search([
+                            ('move_type', '=', 'out_invoice'),
+                            ('partner_id', '=', rec.partner_id.id),
+                            ('state', '=', 'draft'),
+                            ('line_ids.sale_line_ids.order_id', '!=', False)
+                        ])
+                        if sale_with_invoice_draft:
+                            draft_order_amount = sum(sale_with_invoice_draft.mapped('amount_total'))
+                            allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount - draft_order_amount
+                            customer_due_amount += draft_order_amount
+                        else:
+                            allowed_amount = rec.partner_id.sales_credit_limit - customer_due_amount
 
-                    if allowed_amount < 0:
-                        raise ValidationError(_(
-                            "Customer Exceeded Sales Credit Limit = %s, Customer Due Amount = %s",
-                            rec.partner_id.sales_credit_limit, customer_due_amount))
-                    if allowed_amount < rec.amount_total:
-                        raise ValidationError(_('Customer Allowed Amount: %s \n ', allowed_amount))
+                        if allowed_amount < 0:
+                            raise ValidationError(_(
+                                "Customer Exceeded Sales Credit Limit = %s, Customer Due Amount = %s",
+                                rec.partner_id.sales_credit_limit, customer_due_amount))
+                        if allowed_amount < rec.amount_total:
+                            raise ValidationError(_('Customer Allowed Amount: %s \n ', allowed_amount))
 
         res = super(AccountMove, self).action_post()
 
