d83f7b70707b7701373a4fcb4259320e5ef42f66
hossam <hossamwalid1997@gmail.com>
2024-01-14 10:39:37 +0200

    [ADD] certificate_report_custom, edit: purchase_discount, multi_branch_base

---
+++ certificate_report_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Certificate And Report",
+    "summary": """""",
+    "version": "16.0.1.0.2",
+    "license": "AGPL-3",
+    "author": "SB",
+    "depends": ["purchase"],
+    "data": [
+        'views/product_template.xml',
+        'views/purchase_order.xml',
+        'report/purchase_order.xml',
+        'views/account_move.xml',
+    ],
+}
