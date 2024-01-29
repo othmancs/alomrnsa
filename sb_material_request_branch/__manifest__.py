5e355fca5effae4f53fa1d09b314f89beab4bc7c
hossamwalid1997 <hossamwalid1997@gmail.com>
2024-01-30 00:07:18 +0200

    [ِADD] sb_internal_transfer_receipt, sb_material_request_branch

---
+++ sb_material_request_branch/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "sb.material.request.branch",
+    "version": "1.0.0.16",
+    "depends": [
+        'multi_branch_base', 'ak_material_request',
+    ],
+    "category": "Inventory",
+    "license": "OPL-1",
+
+    "data": [
+        'views/stock_location.xml',
+        'views/material_request.xml',
+    ],
+    "installable": True,
+    "auto_install": False,
+}
