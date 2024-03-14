4745a35233bd5118c2ea5fdecb3ea53b84f46fd0
othmancs <61784613+othmancs@users.noreply.github.com>
2024-03-14 11:09:41 +0300

    Delete ak_material_request directory
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- ak_material_request/models/stock_quant.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api
-
-
-class StockQuant(models.Model):
-    _inherit = 'stock.quant'
-
-    parent_location_id = fields.Many2one(related='location_id.location_id')
