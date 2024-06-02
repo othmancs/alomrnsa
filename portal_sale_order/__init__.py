57bf93de39f6d65b812e7309a23e49e22430e4cf
hossamwalid1997 <hossamwalid1997@gmail.com>
2024-06-02 19:44:45 +0300

    [FIX] portal_sale_order

--- portal_sale_order/__init__.py
+++ portal_sale_order/__init__.py
@@ -5,5 +5,5 @@ from odoo.tools import column_exists, create_column
 
 
 def pre_init_hook(cr):
-    if not column_exists(cr.env.cr, "res_users", "cannot_edit_unit_price"):
-        create_column(cr.env.cr, "res_users", "cannot_edit_unit_price", "bool")
+    if not column_exists(cr, "res_users", "cannot_edit_unit_price"):
+        create_column(cr, "res_users", "cannot_edit_unit_price", "bool")
