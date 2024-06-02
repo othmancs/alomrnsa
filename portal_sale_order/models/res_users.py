c9d76d33d6a06bec324017001aa636d4106f0495
hossamwalid1997 <hossamwalid1997@gmail.com>
2024-06-02 19:45:09 +0300

    [FIX] portal_sale_order

--- portal_sale_order/models/res_users.py
+++ portal_sale_order/models/res_users.py
@@ -6,10 +6,3 @@ class ResUsers(models.Model):
     _inherit = 'res.users'
 
     cannot_edit_unit_price = fields.Boolean('عدم تعديل السعر')
-
-    def _auto_init(self):
-        # Skip the computation of the field `livechat_operator_id` at the module installation
-        # We can assume no livechat operator attributed to visitor if it was not installed
-        if not column_exists(self.env.cr, "res_users", "cannot_edit_unit_price"):
-            create_column(self.env.cr, "res_users", "cannot_edit_unit_price", "bool")
-        return super()._auto_init()
