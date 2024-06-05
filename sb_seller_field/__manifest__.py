b1f767419da7708443f655c8854dc64253e57203
andrewnashaat12 <dodododo2332dodo@gmail.com>
2024-06-05 12:25:25 +0300

    [ADD] sb_seller_field

---
+++ sb_seller_field/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "SB Seller Field",
+    "version": "1.0.0.16",
+    "depends": [
+        'base', 'sale','multi_branch_base'
+    ],
+    "data": [
+        'views/sale_order.xml',
+    ],
+    "installable": True,
+    "auto_install": False,
+}
