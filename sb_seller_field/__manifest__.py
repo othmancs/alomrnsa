092b65daac6e9abe6ccb82335c05ec087e07cac8
andrewnashaat12 <dodododo2332dodo@gmail.com>
2024-06-23 12:33:28 +0300

    [IMP] sb_seller_field

--- sb_seller_field/__manifest__.py
+++ sb_seller_field/__manifest__.py
@@ -4,10 +4,14 @@
     "name": "SB Seller Field",
     "version": "1.0.0.16",
     "depends": [
-        'base', 'sale','multi_branch_base'
+        'base', 'sale', 'multi_branch_base','stock','ak_material_request'
     ],
     "data": [
+        'views/material_request.xml',
+        'views/stock_inventory.xml',
+        'views/stock_picking.xml',
         'views/sale_order.xml',
+
     ],
     "installable": True,
     "auto_install": False,
---
+++ sb_seller_field/model/material_request.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class MaterialRequest(models.Model):
+    _inherit = 'material.request'
+
+    created_by_id = fields.Many2one('res.partner', string='انشأ من قبل')
+
