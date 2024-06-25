d91fd7ff68302e9024a9dbdad4d7f64ca984adaa
kareemmostafaa <pr.kareem.mostafa@gmail.com>
2024-06-25 11:57:53 +0300

    hide_inventory_types

--- hide_inventory_types/models/models.py
+++ hide_inventory_types/models/models.py
@@ -13,7 +13,7 @@ class StockType(models.Model):
     _inherit = 'stock.picking.type'
 
     user_ids = fields.Many2many(comodel_name="res.users", string="Users", )
-    current_user_id = fields.Many2one('res.users', compute='_compute_current_user', string='Current User', store=True)
+    current_user_id = fields.Many2one('res.users', compute='_compute_current_user', string='Current User')
 
     def _compute_current_user(self):
         for record in self:
