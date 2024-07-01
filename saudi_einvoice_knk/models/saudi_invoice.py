de01b089ab67aeb1251fd8748239a0f0800603a0
othmancs <61784613+othmancs@users.noreply.github.com>
2024-07-01 14:03:12 +0300

    Update saudi_invoice.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- saudi_einvoice_knk/models/saudi_invoice.py
+++ saudi_einvoice_knk/models/saudi_invoice.py
@@ -74,7 +74,7 @@ class AccountMove(models.Model):
         after_point_value = amount_str_splt[1][:2]
         before_amount_words = num2words(int(before_point_value), lang=language)
         after_amount_words = num2words(int(after_point_value), lang=language)
-        amount = before_amount_words + ' ' + after_amount_words
+        amount = before_amount_words + 'و' + after_amount_words
         return amount
 
     def amount_total_words(self, amount):
