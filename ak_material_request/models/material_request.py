d7fac4e8d97e3e73ef80292da89836c825f9d315
mahmoud-badri <eng.mahmoud.badrii@gmail.com>
2024-07-04 11:12:15 +0300

    [IMP] ak_material_request

--- ak_material_request/models/material_request.py
+++ ak_material_request/models/material_request.py
@@ -48,13 +48,13 @@ class MaterialRequest(models.Model):
     branch_from_id = fields.Many2one(
         'res.branch',
         string='من فرع',
-        domain=lambda self: [('id', 'in', self.env.user.branch_ids.ids)],
+        # domain=lambda self: [('id', 'in', self.env.user.branch_ids.ids)],
         default=lambda self: self.env.user.branch_id.id,
     )
     branch_to_id = fields.Many2one(
         'res.branch',
         string='الى فرع',
-        domain=lambda self: [('id', 'in', self.env.user.branch_ids.ids)],
+        # domain=lambda self: [('id', 'in', self.env.user.branch_ids.ids)],
         default=lambda self: self.env.user.branch_id.id,
     )
     dest_location_id = fields.Many2one(
