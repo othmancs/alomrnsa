cb03978bccd9ddc9fa04a193fa2b0f4760a9c02a
mahmoud-badri <eng.mahmoud.badrii@gmail.com>
2024-07-24 13:48:20 +0300

    [IMP] sb_total_seller_activity_report

--- sb_total_seller_activity_report/__manifest__.py
+++ sb_total_seller_activity_report/__manifest__.py
@@ -5,7 +5,7 @@
     'depends': ['base', 'sale', 'multi_branch_base', 'account', 'sb_seller_field', 'report_xlsx', 'sb_sale_edit_and_reports'],
     'data': [
         'security/ir.model.access.csv',
-        'wizard/sales_per_day.xml',
-        'reports/sales_per_day_report.xml',
+        'wizard/total_seller_activity.xml',
+        'reports/seller_activity_report.xml',
     ],
 }
