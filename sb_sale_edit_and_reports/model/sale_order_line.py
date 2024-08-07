abb60e6e4b3acb4c7afb7c035bb9edf02f513c76
othmancs <61784613+othmancs@users.noreply.github.com>
2024-08-07 09:50:41 +0300

    Update sale_order_line.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/sale_order_line.py
+++ sb_sale_edit_and_reports/model/sale_order_line.py
@@ -1,9 +1,9 @@
 from odoo import fields, models
 
+
+
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
-
-    purchase_price = fields.Float(string='Purchase Price')
 
     def _prepare_invoice_line(self, **optional_values):
         invoice_vals = super(SaleOrderLine, self)._prepare_invoice_line()
