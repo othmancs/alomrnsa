e496c2dff09d7fdc7b251c41a32dcb0bc96d31b8
andrewnashaat12 <dodododo2332dodo@gmail.com>
2024-08-14 16:01:46 +0300

    [FIX]sb_quantities_sold_report

--- sb_quantities_sold_report/wizard/quantities_sold_wizard.py
+++ sb_quantities_sold_report/wizard/quantities_sold_wizard.py
@@ -25,16 +25,16 @@ class BranchSalesComparison(models.TransientModel):
                   ('invoice_date', '<=', self.date_end),
                   ('state', '=', 'posted'),
                   ('move_type', '=', 'out_invoice'),
-                  # ('line_ids.product_id.categ_id.id','=',obj.product_category_id.ids),
+                  # ('invoice_line_ids.product_id.categ_id.id','=',obj.product_category_id.ids),
                   ('branch_id', '=', self.branch_id.ids)
                   ]
         if len(self.product_category_id) == 1 and self.product_category_id.name == 'All':
-            domain.append(('line_ids.product_id.categ_id.parent_id.name', '=', 'All'))
+            domain.append(('invoice_line_ids.product_id.categ_id.parent_id.name', '=', 'All'))
         else:
-            domain.append(('line_ids.product_id.categ_id.id', 'in', self.product_category_id.ids))
+            domain.append(('invoice_line_ids.product_id.categ_id.id', 'in', self.product_category_id.ids))
         lines_data = self.env['account.move'].search(domain)
         existing_branch = lines_data.mapped('branch_id')
-        existing_products = list(set(lines_data.mapped('line_ids.product_id')))
+        existing_products = list(set(lines_data.mapped('invoice_line_ids.product_id')))
         print('existing_branch', existing_branch)
         report_data = []
 
@@ -47,15 +47,17 @@ class BranchSalesComparison(models.TransientModel):
                 product_name = product.name
                 product_uom_name = ' حبه'
                 product_qty = sum(
-                    current_branch_lines.line_ids.filtered(lambda x: x.product_id == product).mapped('quantity'))
+                    current_branch_lines.invoice_line_ids.filtered(lambda x: x.product_id == product).mapped('quantity'))
                 domain_qty_all = [
                     ('state', '=', 'posted'),
                     ('move_type', '=', 'out_invoice'),
-                    ('line_ids.product_id', '=', product.id)
+                    ('invoice_line_ids.product_id', '=', product.id),
+                    ('branch_id', '=', branch.id)
+
                 ]
                 qty_all = self.env['account.move'].search(domain_qty_all)
                 product_qty_all = sum(
-                    qty_all.line_ids.filtered(lambda x: x.product_id == product).mapped('quantity'))
+                    qty_all.invoice_line_ids.filtered(lambda x: x.product_id == product).mapped('quantity'))
                 domain_on_hand = [
                     # ('date','<',obj.date_start),
                     ('location_id.branch_id', '=', branch.id),
