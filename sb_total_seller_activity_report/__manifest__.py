73d3694d6785e5a8762e86de35be63fff81b47de
andrewnashaat12 <dodododo2332dodo@gmail.com>
2024-08-15 10:43:47 +0300

    [FIX]sb_total_seller_activity_report

--- sb_total_seller_activity_report/__manifest__.py
+++ sb_total_seller_activity_report/__manifest__.py
@@ -5,7 +5,7 @@
     'depends': ['base', 'sale', 'multi_branch_base', 'account', 'sb_seller_field', 'report_xlsx', 'sb_sale_edit_and_reports'],
     'data': [
         'security/ir.model.access.csv',
-        'wizard/total_seller_activity.xml.xml',
+        'wizard/total_seller_activity.xml',
         'reports/seller_activity_report.xml',
     ],
 }
