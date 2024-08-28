676ab77be146e680da585f102ba29e71de3aef30
othmancs <61784613+othmancs@users.noreply.github.com>
2024-08-28 16:25:38 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/sale_order.py
+++ sb_sale_edit_and_reports/model/sale_order.py
@@ -1,5 +1,13 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import UserError
 
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    payment_method = fields.Selection([
+        ('option1', 'نقدى'),
+        ('option2', 'اجل'),
+    ], string='طريقه الدفع', required=True)
 
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
@@ -8,18 +16,48 @@ class SaleOrder(models.Model):
         ('option1', 'نقدى'),
         ('option2', 'اجل'),
     ], string='طريقه الدفع', required=True)
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
 
-    payment_method = fields.Selection([
-        ('option1', 'نقدى'),
-        ('option2', 'اجل'),
-    ], string='طريقه الدفع', required=True)
+    @api.model
+    def create(self, vals):
+        partner = self.env['res.partner'].browse(vals.get('partner_id'))
+        partner.payment_method = vals.get('payment_method')
+        return super(SaleOrder, self).create(vals)
+
     def _prepare_invoice(self):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
         invoice_vals['payment_method'] = self.payment_method
         invoice_vals['created_by_id'] = self.created_by_id.id
         return invoice_vals
+
+class Contract(models.Model):
+    _inherit = 'contract.contract'
+
+    payment_method = fields.Selection(related='partner_id.payment_method', string='طريقه الدفع', store=True)
+
+
+
+# from odoo import fields, models
+
+
+# class SaleOrder(models.Model):
+#     _inherit = 'sale.order'
+
+#     payment_method = fields.Selection([
+#         ('option1', 'نقدى'),
+#         ('option2', 'اجل'),
+#     ], string='طريقه الدفع', required=True)
+# class ResPartner(models.Model):
+#     _inherit = 'res.partner'
+
+#     payment_method = fields.Selection([
+#         ('option1', 'نقدى'),
+#         ('option2', 'اجل'),
+#     ], string='طريقه الدفع', required=True)
+#     def _prepare_invoice(self):
+#         invoice_vals = super(SaleOrder, self)._prepare_invoice()
+#         invoice_vals['payment_method'] = self.payment_method
+#         invoice_vals['created_by_id'] = self.created_by_id.id
+#         return invoice_vals
 
 
 
