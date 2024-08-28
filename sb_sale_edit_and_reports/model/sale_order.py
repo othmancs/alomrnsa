207d15325c44f3ba344d0fcf6223f5e824a837d6
othmancs <61784613+othmancs@users.noreply.github.com>
2024-08-28 16:21:12 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/sale_order.py
+++ sb_sale_edit_and_reports/model/sale_order.py
@@ -8,12 +8,19 @@ class SaleOrder(models.Model):
         ('option1', 'نقدى'),
         ('option2', 'اجل'),
     ], string='طريقه الدفع', required=True)
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
 
+    payment_method = fields.Selection([
+        ('option1', 'نقدى'),
+        ('option2', 'اجل'),
+    ], string='طريقه الدفع', required=True)
     def _prepare_invoice(self):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
         invoice_vals['payment_method'] = self.payment_method
         invoice_vals['created_by_id'] = self.created_by_id.id
         return invoice_vals
+
 
 
 
