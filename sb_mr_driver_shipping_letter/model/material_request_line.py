7a7d384c12b8c401c855d92c5bb0309740a7eb80
andrewnashaat12 <dodododo2332dodo@gmail.com>
2024-09-01 13:49:31 +0300

    [IMP]sb_mr_driver_shipping_letter

--- sb_mr_driver_shipping_letter/model/material_request_line.py
+++ sb_mr_driver_shipping_letter/model/material_request_line.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from odoo import api, fields, models
+from odoo.api import onchange
 
 
 class MRLine(models.Model):
     _inherit = "material.request.line"
 
     seq = fields.Integer(string="التسلسل", required=False, )
+    product_ref= fields.Integer(string="الكود", required=False, )
+    product_ref_2= fields.Char(string="الكود", compute="_compute_product_ref_2", required=False, store='1')
+
+
+    @api.onchange('product_id')
+    def _compute_product_ref_2(self):
+        for rec in self:
+            rec.product_ref_2 = rec.product_id.default_code
 
 
 
