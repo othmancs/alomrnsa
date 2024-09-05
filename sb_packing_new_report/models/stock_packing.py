3835adbb21efcde0dbc246e665f8d3d3758df05d
andrewnashaat12 <dodododo2332dodo@gmail.com>
2024-09-05 10:08:48 +0300

    [IMP]sb_ak_material_report[ADD]sb_packing_new_report

---
+++ sb_packing_new_report/models/stock_packing.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo, Aktiv Software PVT. LTD.
+# See LICENSE file for full copyright & licensing details.
+
+from odoo import models, fields, api, _
+from datetime import datetime
+from odoo.exceptions import UserError, ValidationError
+
+
+class StockPacking(models.Model):
+    _inherit = "stock.picking"
+
+
