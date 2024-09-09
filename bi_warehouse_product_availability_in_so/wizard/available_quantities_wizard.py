c5f6616898d069634012713b0ea9bd217b5c0e8e
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-10 01:16:57 +0300

    Update available_quantities_wizard.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- bi_warehouse_product_availability_in_so/wizard/available_quantities_wizard.py
+++ bi_warehouse_product_availability_in_so/wizard/available_quantities_wizard.py
@@ -10,16 +10,46 @@ class AvailableQuantitiesWizard(models.TransientModel):
     stock_product_id = fields.Many2one("product.product", string="Product", readonly=True)
     stock_location_id = fields.Many2many("stock.quant")
 
+    @api.onchange('stock_product_id')
+    def _onchange_stock_product_id(self):
+        if self.stock_product_id:
+            stock_quant_id = self.env['stock.quant'].search([('product_id','=',self.stock_product_id.id),('location_id.usage','=','internal')])
+            product_details = [product.id for product in stock_quant_id]
+            self.stock_location_id = [(6, 0, product_details)]
+
     @api.model
     def default_get(self, fields):
         res = super(AvailableQuantitiesWizard, self).default_get(fields)
         sale_order_line_id = self.env['sale.order.line'].browse(self._context.get('active_id'))
-        stock_quant_id = self.env['stock.quant'].search([('product_id','=',sale_order_line_id.product_id.id),('location_id.usage','=','internal')])
-        product_details = []
-        for product in stock_quant_id:
-            product_details.append(product.id)
-        res = {
-                'stock_product_id' : sale_order_line_id.product_id.id,
-                'stock_location_id' : [(6,0,product_details)]
-                }
+        res.update({
+            'stock_product_id': sale_order_line_id.product_id.id,
+        })
         return res
+
+
+
+# -*- coding: utf-8 -*-
+# Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
+
+# from odoo import models, fields, api, _
+
+# class AvailableQuantitiesWizard(models.TransientModel):
+#     _name = 'available.quantities.wizard'
+#     _description = 'Available Quantity Wizard'
+
+#     stock_product_id = fields.Many2one("product.product", string="Product", readonly=True)
+#     stock_location_id = fields.Many2many("stock.quant")
+
+#     @api.model
+#     def default_get(self, fields):
+#         res = super(AvailableQuantitiesWizard, self).default_get(fields)
+#         sale_order_line_id = self.env['sale.order.line'].browse(self._context.get('active_id'))
+#         stock_quant_id = self.env['stock.quant'].search([('product_id','=',sale_order_line_id.product_id.id),('location_id.usage','=','internal')])
+#         product_details = []
+#         for product in stock_quant_id:
+#             product_details.append(product.id)
+#         res = {
+#                 'stock_product_id' : sale_order_line_id.product_id.id,
+#                 'stock_location_id' : [(6,0,product_details)]
+#                 }
+#         return res
