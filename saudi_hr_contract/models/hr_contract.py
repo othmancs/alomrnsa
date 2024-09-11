ed13cde9f4a07f9504e87e5c4fdfd466c8711927
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-11 22:59:00 +0300

    Update hr_contract.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- saudi_hr_contract/models/hr_contract.py
+++ saudi_hr_contract/models/hr_contract.py
@@ -9,6 +9,7 @@ from odoo.exceptions import UserError
 class HRContract(models.Model):
     _name = 'hr.contract'
     _inherit = ['mail.thread', 'hr.contract']
+    schedule_pay = fields.Selection(related='structure_type_id.default_struct_id.schedule_pay',string='Schedule Pay',store=True)
 
     signon_bonus = fields.Boolean('Sign on Bonus')
     signon_bonus_amount = fields.Float('Bonus Amount', digits=(16, 2), help="Mention the Sign on Bonus amount.")
