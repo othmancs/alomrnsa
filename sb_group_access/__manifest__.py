6fe19bef8aa24bdc558b1a989e0b08a4cf48d780
mahmoud-badri <eng.mahmoud.badrii@gmail.com>
2024-09-12 11:19:57 +0300

    [IMP] sb_group_access

--- sb_group_access/__manifest__.py
+++ sb_group_access/__manifest__.py
@@ -19,7 +19,6 @@
         'security/sb_product_group_access.xml',
         'security/sb_inventory_group_access.xml',
         'security/stock_picking_security.xml',
-        # 'views/stock_picking_views.xml',
         'views/sale_order.xml',
         'views/account_move.xml',
 
--- sb_group_access/model/stock_picking.py
+++ sb_group_access/model/stock_picking.py
@@ -25,7 +25,6 @@ class StockPickingSecurity(models.Model):
         if 'move_ids_without_package' in vals and self.state != 'draft':
             # Check if user has the specific group permissions
             if not self.env.user.has_group('sb_group_access.group_inv_transfer_add_line'):
-                # Check for new line additions in move_ids_without_package
                 new_lines = vals.get('move_ids_without_package', [])
                 if isinstance(new_lines, list) and any(line[0] == 0 for line in new_lines):
                     raise UserError(_("لا يمكنـك إضــافـة أصنــاف أخــرى"))
