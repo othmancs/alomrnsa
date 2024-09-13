19d5d6eeb6e26d11561dc93632fd811f7dfa7dc8
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-13 15:46:45 +0300

    Update product_category.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customization_alomran/models/product_category.py
+++ customization_alomran/models/product_category.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models
 class ProductCategory(models.Model):
     _inherit = 'product.category'
 
-    stock_warehouse_id = fields.Many2one('stock.warehouse', string='Stock Warehouse')
+    stock_warehouse_ids = fields.Many2many('stock.warehouse', string='Stock Warehouses')
