2bc457b3d90900ed94dafac067d0961535df2b0f
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-14 04:15:20 +0300

    Update res_partner.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- res_partner_cs/models/res_partner.py
+++ res_partner_cs/models/res_partner.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    customer_type = fields.Selection([
+    customer_type2 = fields.Selection([
         ('cash', 'Cash'),
         ('credit', 'Credit')
     ], string='Customer Type', default='cash')
