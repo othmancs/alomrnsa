3b0a1ca612c0a66a9ac27572f9d01d1bcaede85e
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-16 00:20:11 +0300

    Update eos_report.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- eos_cs2/models/eos_report.py
+++ eos_cs2/models/eos_report.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from datetime import date
 
 class Employee(models.Model):
     _inherit = 'hr.employee'
@@ -7,16 +8,18 @@ class Employee(models.Model):
     service_years = fields.Float(string='Service Years', compute='_compute_service_years')
     basic_salary = fields.Monetary(string='Basic Salary', related='employee_id.contract_id.wage', currency_field='currency_id')
     eos_amount = fields.Monetary(string='End of Service Amount', compute='_compute_eos_amount', currency_field='currency_id')
+    structure_type_id = fields.Many2one('hr.payroll.structure.type', string='Structure Type')
     schedule_pay = fields.Selection(related='structure_type_id.default_struct_id.schedule_pay', depends=())
-    currency_id = fields.Many2one('res.currency', string='Currency')
-    structure_type_id = fields.Many2one('hr.payroll.structure.type', string='Structure Type')  # تأكد من وجود هذا الحقل
 
     @api.depends('employee_id')
     def _compute_service_years(self):
         for record in self:
             start_date = record.employee_id.contract_id.date_start
-            end_date = fields.Date.today()
-            record.service_years = (end_date - start_date).days / 365.0
+            end_date = date.today()
+            if start_date and isinstance(start_date, date):
+                record.service_years = (end_date - start_date).days / 365.0
+            else:
+                record.service_years = 0.0
 
     @api.depends('employee_id')
     def _compute_eos_amount(self):
