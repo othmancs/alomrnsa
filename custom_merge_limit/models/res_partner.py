03765f7be08560a19678404aea7d811136be6e48
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-16 15:11:27 +0300

    Update res_partner.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- custom_merge_limit/models/res_partner.py
+++ custom_merge_limit/models/res_partner.py
@@ -1,14 +1,13 @@
-# custom_merge_limit/models/res_partner.py
-
 from odoo import models, api
 from odoo.exceptions import UserError
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    @api.multi
-    def merge_contacts(self):
+    @api.model
+    def merge_contacts(self, contacts):
         # تعديل الحد المسموح
-        if len(self) > 10:  # تغيير 10 إلى الرقم الذي تريده
+        if len(contacts) > 10:  # تغيير 10 إلى الرقم الذي تريده
             raise UserError("لا يمكنك دمج أكثر من 10 جهات اتصال معاً.")
-        super(ResPartner, self).merge_contacts()
+        # افترض أن هناك منطق الدمج هنا
+        # super(ResPartner, self).merge_contacts()
