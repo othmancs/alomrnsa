8002aaab92628cd526cc2b48f45a3a9af29a351b
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-21 16:29:48 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customer_fields_sale_invoice/models/sale_order.py
+++ customer_fields_sale_invoice/models/sale_order.py
@@ -3,20 +3,19 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    customer_name = fields.Char(string="اسم العميل")
+  customer_name = fields.Char(string="اسم العميل")
     customer_phone = fields.Char(string="رقم الهاتف")
 
     def _prepare_invoice(self):
-        """إضافة الحقول الخاصة باسم العميل ورقم هاتفه عند إنشاء الفاتورة"""
-        # استدعاء الدالة الأصلية مرة واحدة فقط
-        invoice_vals = super(SaleOrder, self)._prepare_invoice()
-
-        # إضافة الحقول الخاصة باسم العميل ورقم الهاتف
+        # استدعاء الدالة من الكلاس المباشر
+        invoice_vals = super(CustomerFieldsSaleInvoice, self)._prepare_invoice()
+        
+        # إضافة الحقول الخاصة بك إلى invoice_vals
         invoice_vals.update({
             'customer_name': self.customer_name,
             'customer_phone': self.customer_phone,
         })
-
+        
         return invoice_vals
 
     def _create_invoice_vals(self):
