7e8d21256fd733cfe771c336d9ca083f254bdd35
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-21 17:10:05 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customer_fields_sale_invoice/models/sale_order.py
+++ customer_fields_sale_invoice/models/sale_order.py
@@ -3,12 +3,12 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-  customer_name = fields.Char(string="اسم العميل")
+    customer_name = fields.Char(string="اسم العميل")
     customer_phone = fields.Char(string="رقم الهاتف")
 
     def _prepare_invoice(self):
         # استدعاء الدالة من الكلاس المباشر
-        invoice_vals = super(CustomerFieldsSaleInvoice, self)._prepare_invoice()
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
         
         # إضافة الحقول الخاصة بك إلى invoice_vals
         invoice_vals.update({
@@ -26,10 +26,8 @@ class SaleOrder(models.Model):
             'customer_phone': self.customer_phone
         }
         return invoice_vals
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
 
     _rec_name = 'customer_name'
 
     def _search_customer_fields(self, operator, value):
-        return ['|', ('customer_name', operator, value), ('customer_phone', operator, value)]
+        return ['|', ('customer_name', operator, value), ('customer_phone', operator, v
