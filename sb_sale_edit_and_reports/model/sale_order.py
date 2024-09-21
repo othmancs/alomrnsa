19716a4d1670bd6498b3546f4a16780b0a2c9f90
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-21 16:49:42 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/sale_order.py
+++ sb_sale_edit_and_reports/model/sale_order.py
@@ -1,6 +1,5 @@
 from odoo import fields, models
 
-
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
@@ -8,15 +7,15 @@ class SaleOrder(models.Model):
         ('option1', 'نقدى'),
         ('option2', 'اجل'),
     ], string='طريقه الدفع', required=True)
+
 class SaleOrderEditReports(models.Model):
     _inherit = 'sale.order'
 
     def _prepare_invoice(self):
-        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals = super(SaleOrderEditReports, self)._prepare_invoice()
+        
+        # إضافة طريقة الدفع و معرف المنشئ إلى invoice_vals
         invoice_vals['payment_method'] = self.payment_method
         invoice_vals['created_by_id'] = self.created_by_id.id
+        
         return invoice_vals
-
-
-
-
