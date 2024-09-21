ba67e76d7aa24d5ab5238bd7203968f3f2a72f24
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-21 17:11:54 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customer_fields_sale_invoice/models/sale_order.py
+++ customer_fields_sale_invoice/models/sale_order.py
@@ -7,19 +7,14 @@ class SaleOrder(models.Model):
     customer_phone = fields.Char(string="رقم الهاتف")
 
     def _prepare_invoice(self):
-        # استدعاء الدالة من الكلاس المباشر
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
-        
-        # إضافة الحقول الخاصة بك إلى invoice_vals
         invoice_vals.update({
             'customer_name': self.customer_name,
             'customer_phone': self.customer_phone,
         })
-        
         return invoice_vals
 
     def _create_invoice_vals(self):
-        """تحديث الحقول في الفاتورة عند إنشائها"""
         self.ensure_one()
         invoice_vals = {
             'customer_name': self.customer_name,
@@ -30,4 +25,4 @@ class SaleOrder(models.Model):
     _rec_name = 'customer_name'
 
     def _search_customer_fields(self, operator, value):
-        return ['|', ('customer_name', operator, value), ('customer_phone', operator, v
+        return ['|', ('customer_name', operator, value), ('customer_phone', operator, value)]
