a10c16b5603c3e51ef76ba04061c5e04dab551c6
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-21 16:19:34 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/sale_order.py
+++ sb_sale_edit_and_reports/model/sale_order.py
@@ -11,6 +11,10 @@ class SaleOrder(models.Model):
 
     def _prepare_invoice(self):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        # تعديل القيم أو الحقول الخاصة بالفواتير
+        return invoice_vals
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
         invoice_vals['payment_method'] = self.payment_method
         invoice_vals['created_by_id'] = self.created_by_id.id
         return invoice_vals
