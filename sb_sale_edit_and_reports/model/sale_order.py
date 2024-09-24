af24b17c40061e72b768199de3903b3f276d7b76
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-24 09:38:59 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/sale_order.py
+++ sb_sale_edit_and_reports/model/sale_order.py
@@ -7,6 +7,8 @@ class SaleOrder(models.Model):
         ('option1', 'نقدى'),
         ('option2', 'اجل'),
     ], string='طريقه الدفع', required=True)
+    customer_name = fields.Char(string="اسم العميل")
+    customer_phone = fields.Char(string="رقم هاتف العميل")
 
     def _prepare_invoice(self):
         invoice_vals = super(SaleOrder, self)._prepare_invoice()
@@ -16,3 +18,11 @@ class SaleOrder(models.Model):
         invoice_vals['created_by_id'] = self.created_by_id.id
         
         return invoice_vals
+    @api.model
+    def _name_search(self, name, args=None, operator='ilike', limit=100, name_get_uid=None):
+        args = args or []
+        domain = ['|', ('customer_name', operator, name), ('customer_phone', operator, name)]
+        return self._search(domain + args, limit=limit, access_rights_uid=name_get_uid)
+
+    # تخصيص الاسم الذي يظهر في القائمة
+    _rec_name = 'customer_name'
