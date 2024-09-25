297e0e4ab233b773b4ce5de0c0bb412d86eec0a5
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-25 04:03:44 +0300

    Update __manifest__.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- hr_employee_contract_sa/__manifest__.py
+++ hr_employee_contract_sa/__manifest__.py
@@ -1,12 +1,18 @@
 {
-    'name' 'Employee Contract (Saudi)',
-    'version' '1.0',
-    'category' 'Human Resources',
-    'summary' 'Module to print employee contracts according to Saudi regulations',
-    'depends' ['hr', 'report'],
-    'data' [
-        'viewsemployee_contract_views.xml',
-        'reportsemployee_contract_report.xml',
-        'securityir.model.access.csv',
+    'name': 'Employee Contract (Saudi)',
+    'version': '1.0',
+    'category': 'Human Resources',
+    'summary': 'Module to print employee contracts according to Saudi regulations',
+    'description': """
+        This module allows the management and printing of employee contracts 
+        according to the labor laws in Saudi Arabia.
+    """,
+    'depends': ['hr', 'report'],
+    'data': [
+        'views/employee_contract_views.xml',
+        'reports/employee_contract_report.xml',
+        'security/ir.model.access.csv',
     ],
+    'installable': True,
+    'application': False,
 }
