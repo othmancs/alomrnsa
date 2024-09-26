4205561299d219ec559231f930a90c0304dc7821
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-26 14:28:40 +0300

    Update sale_order_custom_fields.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- custom_sale_order_fields/models/sale_order_custom_fields.py
+++ custom_sale_order_fields/models/sale_order_custom_fields.py
@@ -3,5 +3,5 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    customer_name = fields.Char(string="اسم العميل")
-    customer_phone = fields.Char(string="رقم الجوال")
+    name_custom  = fields.Char(string="اسم العميل")
+    num_custom  = fields.Char(string="رقم الجوال")
