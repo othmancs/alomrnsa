191db12710cc3a6448959f7b61d817fed7f32c66
othmancs <61784613+othmancs@users.noreply.github.com>
2024-09-29 21:17:06 +0300

    Update account_move.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- draf_invice_cs/models/account_move.py
+++ draf_invice_cs/models/account_move.py
@@ -1,25 +1,25 @@
-# from odoo import models, api
-# from odoo.exceptions import UserError
-
-# class AccountMove(models.Model):
-#     _inherit = 'account.move'
-
-#     def action_reset_to_draft(self):
-#         for move in self:
-#             # تحقق من أن الفاتورة في حالة منشورة
-#             if move.state != 'posted':
-#                 raise UserError('Only posted invoices can be reset to draft.')
-#             # تغيير الحالة إلى "مسودة"
-#             move.write({'state': 'draft'})
-#             # إلغاء ترحيل القيود المحاسبية المرتبطة بالفاتورة
-#             move.line_ids.remove_move_reconcile()
 from odoo import models, api
+from odoo.exceptions import UserError
 
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    @api.multi
     def action_reset_to_draft(self):
-        for invoice in self:
-            if invoice.state == 'posted':
-                invoice.button_draft()
+        for move in self:
+            # تحقق من أن الفاتورة في حالة منشورة
+            if move.state != 'posted':
+                raise UserError('Only posted invoices can be reset to draft.')
+            # تغيير الحالة إلى "مسودة"
+            move.write({'state': 'draft'})
+            # إلغاء ترحيل القيود المحاسبية المرتبطة بالفاتورة
+            move.line_ids.remove_move_reconcile()
+# from odoo import models, api
+
+# class AccountMove(models.Model):
+#     _inherit = 'account.move'
+
+#     @api.multi
+#     def action_reset_to_draft(self):
+#         for invoice in self:
+#             if invoice.state == 'posted':
+#                 invoice.button_draft()
