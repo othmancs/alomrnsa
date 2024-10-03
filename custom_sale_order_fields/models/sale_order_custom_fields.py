28bd6de93884a54dbea12e23ede0fe0b8024b57b
othmancs <61784613+othmancs@users.noreply.github.com>
2024-10-03 10:28:09 +0300

    Update sale_order_custom_fields.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- custom_sale_order_fields/models/sale_order_custom_fields.py
+++ custom_sale_order_fields/models/sale_order_custom_fields.py
@@ -5,10 +5,19 @@ class SaleOrder(models.Model):
 
     name_custom  = fields.Char(string="اسم العميل")
     num_custom  = fields.Char(string="رقم الجوال")
+
+ @api.model
+    def _prepare_invoice(self):
+        invoice_vals = super(SaleOrder, self)._prepare_invoice()
+        invoice_vals.update({
+            'name_custom': self.name_custom,
+            'num_custom': self.num_custom,
+        })
+        return invoice_vals
+        
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
     # إضافة الحقول المخصصة للفواتير
-    sale_order_id = fields.Many2one('sale.order', string='طلب البيع الأصلي')
     name_custom = fields.Char(string="اسم العميل", related='sale_order_id.name_custom')
     num_custom = fields.Char(string="رقم الجوال", related='sale_order_id.num_custom')
