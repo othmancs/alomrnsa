67ceb0b64850714ab59166613cca7a8b444d62ed
othmancs <61784613+othmancs@users.noreply.github.com>
2024-10-03 10:30:32 +0300

    Update sale_order_custom_fields.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- custom_sale_order_fields/models/sale_order_custom_fields.py
+++ custom_sale_order_fields/models/sale_order_custom_fields.py
@@ -6,18 +6,18 @@ class SaleOrder(models.Model):
     name_custom  = fields.Char(string="اسم العميل")
     num_custom  = fields.Char(string="رقم الجوال")
 
- @api.model
-    def _prepare_invoice(self):
-        invoice_vals = super(SaleOrder, self)._prepare_invoice()
-        invoice_vals.update({
-            'name_custom': self.name_custom,
-            'num_custom': self.num_custom,
-        })
-        return invoice_vals
+#  @api.model
+#     def _prepare_invoice(self):
+#         invoice_vals = super(SaleOrder, self)._prepare_invoice()
+#         invoice_vals.update({
+#             'name_custom': self.name_custom,
+#             'num_custom': self.num_custom,
+#         })
+#         return invoice_vals
         
-class AccountMove(models.Model):
-    _inherit = 'account.move'
+# class AccountMove(models.Model):
+#     _inherit = 'account.move'
 
-    # إضافة الحقول المخصصة للفواتير
-    name_custom = fields.Char(string="اسم العميل", related='sale_order_id.name_custom')
-    num_custom = fields.Char(string="رقم الجوال", related='sale_order_id.num_custom')
+#     # إضافة الحقول المخصصة للفواتير
+#     name_custom = fields.Char(string="اسم العميل", related='sale_order_id.name_custom')
+#     num_custom = fields.Char(string="رقم الجوال", related='sale_order_id.num_custom')
