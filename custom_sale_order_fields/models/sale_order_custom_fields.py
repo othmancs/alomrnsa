707e0b5c336e564be892aa40268b133fbd3515bc
othmancs <61784613+othmancs@users.noreply.github.com>
2024-10-03 10:15:34 +0300

    Update sale_order_custom_fields.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- custom_sale_order_fields/models/sale_order_custom_fields.py
+++ custom_sale_order_fields/models/sale_order_custom_fields.py
@@ -5,3 +5,10 @@ class SaleOrder(models.Model):
 
     name_custom  = fields.Char(string="اسم العميل")
     num_custom  = fields.Char(string="رقم الجوال")
+class AccountMove(models.Model):
+    _inherit = 'account.move'
+
+    # إضافة الحقول المخصصة للفواتير
+    sale_order_id = fields.Many2one('sale.order', string='طلب البيع الأصلي')
+    name_custom = fields.Char(string="اسم العميل", related='sale_order_id.name_custom')
+    num_custom = fields.Char(string="رقم الجوال", related='sale_order_id.num_custom')
