3100c8a268415274a80eb7284054eb6b21eb4d4d
othmancs <61784613+othmancs@users.noreply.github.com>
2024-10-08 15:11:47 +0300

    Update stock_picking.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_delivery/models/stock_picking.py
+++ restrict_delivery/models/stock_picking.py
@@ -9,7 +9,7 @@ class StockPicking(models.Model):
         for picking in self:
             for move in picking.move_lines:
                 sale_order_line = move.sale_line_id
-                if sale_order_line and move.product_uom_qty > sale_order_line.product_uom_qty:
+                if sale_order_line and move.quantity_done > sale_order_line.product_uom_qty:
                     raise ValidationError(
                         f"لا يمكن تسليم كمية أكبر من الكمية المطلوبة ({sale_order_line.product_uom_qty}) لمنتج {move.product_id.name}."
                     )
