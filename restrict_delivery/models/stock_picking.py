2b4cecf1ff9a6ff44c19367a0972849cfaa5a37b
othmancs <61784613+othmancs@users.noreply.github.com>
2024-10-08 16:06:27 +0300

    Update stock_picking.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_delivery/models/stock_picking.py
+++ restrict_delivery/models/stock_picking.py
@@ -1,37 +1,15 @@
-from odoo import models, api, fields
-from odoo.exceptions import ValidationError, UserError
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
-# class StockMove(models.Model):
-#     _inherit = 'stock.move'
-
-#     @api.constrains('quantity_done')
-#     def _check_quantity_done(self):
-#         """منع التصديق أو الحفظ إذا كانت الكمية المنفذة أكبر من الكمية المطلوبة في الحركة."""
-#         for move in self:
-#             if move.quantity_done > move.product_uom_qty:
-#                 raise ValidationError(
-#                     f"لا يمكن التصديق لأن الكمية المنفذة ({move.quantity_done}) أكبر من الكمية المحددة ({move.product_uom_qty}) للمنتج {move.product_id.name}."
-#                 )
-
-#     def button_validate(self):
-#         """التأكد من عدم تجاوز الكمية قبل التصديق."""
-#         self._check_quantity_done()  # تأكد من التحقق قبل التصديق
-#         return super(StockMove, self).button_validate()  # اتصل بإجراء التصديق الأساسي
-
-                    )
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    @api.model
-    def create(self, vals):
-        """منع إضافة منتجات جديدة إلى خطوط حركة المخزون بعد تأكيد أمر المبيعات."""
-        picking = super(StockPicking, self).create(vals)
-        if picking.sale_id and picking.sale_id.state in ['sale', 'done']:
-            raise UserError('لا يمكنك إضافة منتجات جديدة بعد تأكيد أمر المبيعات.')
-        return picking
-
-    def write(self, vals):
-        """منع تعديل خطوط حركة المخزون بعد تأكيد أمر المبيعات."""
-        if 'move_lines' in vals and self.sale_id and self.sale_id.state in ['sale', 'done']:
-            raise UserError('لا يمكنك تعديل المنتجات بعد تأكيد أمر المبيعات.')
-        return super(StockPicking, self).write(vals)
+    @api.constrains('move_lines')
+    def _check_delivery_quantity(self):
+        for picking in self:
+            for move in picking.move_lines:
+                sale_order_line = move.sale_line_id
+                if sale_order_line and move.product_uom_qty > sale_order_line.product_uom_qty:
+                    raise ValidationError(
+                        f"لا يمكن تسليم كمية أكبر من الكمية المطلوبة ({sale_order_line.product_uom_qty}) لمنتج {move.product_id.name}."
+                    )
