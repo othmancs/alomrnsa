ffd7c40753622747ed96ca1459085dbd16db612f
othmancs <61784613+othmancs@users.noreply.github.com>
2024-11-09 14:43:38 +0300

    Delete saudi_hr_contract directory
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- saudi_hr_contract/models/related_model.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class RelatedModel(models.Model):
-    _name = 'related.model'
-    _description = 'Related Model'
-    
-    schedule_pay = fields.Char(string="Payment Schedule")
