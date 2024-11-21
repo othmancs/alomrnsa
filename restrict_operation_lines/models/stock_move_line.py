36d5331c4b913809c64c8ae98ab0dbcd0adb1d90
othmancs <61784613+othmancs@users.noreply.github.com>
2024-11-21 10:00:44 +0300

    Update stock_move_line.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_operation_lines/models/stock_move_line.py
+++ restrict_operation_lines/models/stock_move_line.py
@@ -9,10 +9,10 @@ class StockPicking(models.Model):
         help="Indicates whether the user has permission to add operation lines."
     )
 
-    @api.depends('id')
+    @api.depends_context('uid')  # يتم تقييمه بناءً على سياق المستخدم
     def _compute_can_add_operation_lines(self):
         for record in self:
-            # Check if the user has the required group
+            # تحقق من وجود المستخدم في المجموعة
             record.can_add_operation_lines = self.env.user.has_group(
                 'restrict_operation_lines.group_restrict_add_operation_lines'
             )
