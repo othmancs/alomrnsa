3a977960ec6704dd73a3169c76a7bb058d706639
othmancs <61784613+othmancs@users.noreply.github.com>
2024-11-21 10:41:21 +0300

    Update stock_move_line.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_operation_lines/models/stock_move_line.py
+++ restrict_operation_lines/models/stock_move_line.py
@@ -1,18 +1,15 @@
-from odoo import models, fields, api
+from odoo import models, fields
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
     can_add_operation_lines = fields.Boolean(
+        string='Can Add Operation Lines',
         compute='_compute_can_add_operation_lines',
-        string="Can Add Operation Lines",
-        help="Indicates whether the user has permission to add operation lines."
+        store=True
     )
 
-    @api.depends_context('uid')  # يتم تقييمه بناءً على سياق المستخدم
     def _compute_can_add_operation_lines(self):
         for record in self:
-            # تحقق من وجود المستخدم في المجموعة
-            record.can_add_operation_lines = self.env.user.has_group(
-                'restrict_operation_lines.group_restrict_add_operation_lines'
-            )
+            # Add your logic here
+            record.can_add_operation_lines = True
