8c80b5344310d9ecb2dd31ffdc3b4e15851dd668
othmancs <61784613+othmancs@users.noreply.github.com>
2024-11-25 00:17:35 +0300

    Update res_partner.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- ae_clear_dup_contacts/models/res_partner.py
+++ ae_clear_dup_contacts/models/res_partner.py
@@ -1,56 +1,19 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#    Copyright (C) 2020 AtharvERP (<http://atharverp.com/>). All Rights Reserved
-# Odoo Proprietary License v1.0
-#
-# This software and associated files (the "Software") may only be used (executed,
-# modified, executed after modifications) if you have purchased a valid license
-# from the authors, typically via Odoo Apps, atharverp.com or you have written agreement from
-# author of this software owner.
-#
-# You may develop Odoo modules that use the Software as a library (typically
-# by depending on it, importing it and using its resources), but without copying
-# any source code or material from the Software. You may distribute those
-# modules under the license of your choice, provided that this license is
-# compatible with the terms of the Odoo Proprietary License (For example:
-# LGPL, MIT, or proprietary licenses similar to this one).
-#
-# It is forbidden to publish, distribute, sublicense, or sell copies of the Software
-# or modified copies of the Software.
-#
-# The above copyright notice and this permission notice must be included in all
-# copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
-##############################################################################
-
 from odoo import models
 
-
 class ResPartnerExt(models.Model):
-
     _inherit = 'res.partner'
     _description = 'Partner'
 
     def clear_duplicates(self):
         duplicate_contacts = []
         user_obj = self.env['res.users']
-cale_obj = self.env.get('calendar.contacts')
-if not cale_obj:
-    # النموذج غير موجود، قم بمعالجة الحالة هنا أو تسجيل رسالة خطأ
-    return
+        cale_obj = self.env.get('calendar.contacts')  # استخدام get لتجنب الخطأ إذا كان النموذج غير موجود
         for partner in self:
             if partner.email and partner.id not in duplicate_contacts:
                 duplicates = self.search([('id', '!=', partner.id), ('email', '=', partner.email)])
                 for dup in duplicates:
                     user = user_obj.search([('partner_id', '=', dup.id)])
-                    calender = cale_obj.search([('partner_id', '=', dup.id)])
+                    calender = cale_obj.search([('partner_id', '=', dup.id)]) if cale_obj else None
                     if not user and not calender:
                         duplicate_contacts.append(dup.id)
         self.browse(duplicate_contacts).unlink()
