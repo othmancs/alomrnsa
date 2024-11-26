30291b878655481161dc7f9532af9159bb3567fd
othmancs <61784613+othmancs@users.noreply.github.com>
2024-11-26 12:17:44 +0300

    Update hr_contract.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- hr_payroll_community/models/hr_contract.py
+++ hr_payroll_community/models/hr_contract.py
@@ -1,5 +1,4 @@
-# -*- coding:utf-8 -*-
-
+# -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
 
@@ -11,24 +10,19 @@ class HrContract(models.Model):
     _inherit = 'hr.contract'
     _description = 'Employee Contract'
 
-    struct_id = fields.Many2one('hr.payroll.structure', string='Salary Structure')
-schedule_pay = fields.Selection(
-        related='contract_id.schedule_pay',  # المسار هنا يجب أن يكون دقيقًا
+    struct_id = fields.Many2one(
+        'hr.payroll.structure', 
+        string='Salary Structure'
+    )
+    schedule_pay = fields.Selection(
+        related='contract_id.schedule_pay',  # تأكد أن المسار صحيح
         string="Schedule Pay",
-        store=True,)
-    # resource_calendar_id = fields.Many2one(required=True, help="Employee's working schedule.")
-    # resource_calendar_id = fields.Many2one('resource.calendar',required=True,help="Employee's working schedule.")
-    # hra = fields.Monetary(string='HRA', tracking=True, help="House rent allowance.")
-    # travel_allowance = fields.Monetary(string="Travel Allowance", help="Travel allowance")
-    # da = fields.Monetary(string="DA", help="Dearness allowance")
-    # meal_allowance = fields.Monetary(string="Meal Allowance", help="Meal allowance")
-    # medical_allowance = fields.Monetary(string="Medical Allowance", help="Medical allowance")
-    # other_allowance = fields.Monetary(string="Other Allowance", help="Other allowances")
+        store=True,
+    )
 
     def get_all_structures(self):
-
         """
-        @return: the structures linked to the given contracts, ordered by hierachy (parent=False first,
+        @return: the structures linked to the given contracts, ordered by hierarchy (parent=False first,
                  then first level children and so on) and without duplicata
         """
         structures = self.mapped('struct_id')
@@ -38,22 +32,18 @@ schedule_pay = fields.Selection(
         return list(set(structures._get_parent_structure().ids))
 
     def get_attribute(self, code, attribute):
-
         return self.env['hr.contract.advantage.template'].search([('code', '=', code)], limit=1)[attribute]
 
     def set_attribute_value(self, code, active):
         for contract in self:
-
             if active:
-
                 value = self.env['hr.contract.advantage.template'].search([('code', '=', code)], limit=1).default_value
                 contract[code] = value
             else:
-
                 contract[code] = 0.0
 
 
-class HrContractAdvandageTemplate(models.Model):
+class HrContractAdvantageTemplate(models.Model):
     _name = 'hr.contract.advantage.template'
     _description = "Employee's Advantage on Contract"
 
