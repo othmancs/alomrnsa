d2d0051e1336f674b130528f3c1481c7a92ed6aa
othmancs <61784613+othmancs@users.noreply.github.com>
2024-11-26 12:10:32 +0300

    Update hr_contract.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- hr_gratuity_settlement/models/hr_contract.py
+++ hr_gratuity_settlement/models/hr_contract.py
@@ -1,25 +1,3 @@
-# -- coding: utf-8 --
-###################################################################################
-#    A part of Open HRMS Project <https://www.openhrms.com>
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2022-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
-#    Author: Cybrosys (<https://www.cybrosys.com>)
-#
-#    This program is free software: you can modify
-#    it under the terms of the GNU Affero General Public License (AGPL) as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-###################################################################################
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
 
@@ -41,45 +19,39 @@ class Probation(models.Model):
     )
     probation_id = fields.Many2one('hr.training')
     half_leave_ids = fields.Many2many('hr.leave', string="Half Leave")
-    training_amount = fields.Float(string='Training Amount', help="amount for the employee during training")
+    training_amount = fields.Float(string='Training Amount', help="Amount for the employee during training")
 
     @api.onchange('trial_date_end')
     def state_probation(self):
         """
-        function used for changing state draft to probation
-        when the end of trail date setting
+        Change state to 'probation' when 'trial_date_end' is set.
         """
-
         if self.trial_date_end:
             self.state = 'probation'
 
     @api.onchange('employee_id')
     def change_employee_id(self):
         """
-        function for changing employee id of hr.training if changed
+        Update the employee_id in the related 'hr.training' record
+        when employee_id changes in the contract.
         """
         if self.probation_id and self.employee_id:
             self.probation_id.employee_id = self.employee_id.id
 
     def action_approve(self):
         """
-        function used for changing the state probation into
-        running when approves a contract
+        Approve the contract and change the state from 'probation' to 'open'.
         """
-
         self.write({'is_approve': True})
         if self.state == 'probation':
-            self.write({'state': 'open',
-                        'is_approve': False})
+            self.write({'state': 'open', 'is_approve': False})
 
     @api.model
     def create(self, vals_list):
         """
-        function for create a record based on probation
-        details in a model
-
+        Create a new probation record in 'hr.training' if the state is 'probation'.
         """
-        if vals_list['trial_date_end'] and vals_list['state'] == 'probation':
+        if vals_list.get('trial_date_end') and vals_list.get('state') == 'probation':
             dtl = self.env['hr.training'].create({
                 'employee_id': vals_list['employee_id'],
                 'start_date': vals_list['date_start'],
@@ -91,15 +63,13 @@ class Probation(models.Model):
 
     def write(self, vals):
         """
-        function for checking stage changing and creating probation
-        record based on contract stage
-
+        Handle state transitions and ensure proper validations for 'probation' contracts.
         """
         if self.state == 'probation':
             if vals.get('state') == 'open' and not self.is_approve:
-                raise UserError(_("You cannot change the status of non-approved Contracts"))
-            if vals.get('state') == 'cancel' or vals.get('state') == 'close' or vals.get('state') == 'draft':
-                raise UserError(_("You cannot change the status of non-approved Contracts"))
+                raise UserError(_("You cannot change the status of non-approved Contracts."))
+            if vals.get('state') in ['cancel', 'close', 'draft']:
+                raise UserError(_("You cannot change the status of non-approved Contracts."))
         training_dtl = self.env['hr.training'].search([('employee_id', '=', self.employee_id.id)])
         if training_dtl:
             return super(Probation, self).write(vals)
