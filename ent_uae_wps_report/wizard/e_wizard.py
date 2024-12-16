995c33638b186a2bb6b4911571ff97581e3010ad
othmancs <61784613+othmancs@users.noreply.github.com>
2024-12-16 14:23:11 +0300

    Update e_wizard.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- ent_uae_wps_report/wizard/e_wizard.py
+++ ent_uae_wps_report/wizard/e_wizard.py
@@ -71,32 +71,32 @@ class Wizard(models.TransientModel):
         users = self.env['hr.employee'].search([])
         flags = {'labour_card_number': True, 'salary_card_number': True,
                  'agent_id': True}
-        for user in users:
-            if not user.labour_card_number:
-                flags['labour_card_number'] = False
-            if not user.salary_card_number:
-                flags['salary_card_number'] = False
-            if not user.agent_id:
-                flags['agent_id'] = False
-        if not flags['labour_card_number']:
-            raise UserError(_('Please Set Labour Card Number of All Employees'))
-        if not flags['salary_card_number']:
-            raise UserError(_('Please Set Salary Card Number / Account Number '
-                              'of All Employees'))
-        if not flags['agent_id']:
-            raise UserError(
-                _('Please Set Employee Card Number of All Employees'))
-        if not self.env['res.users'].browse(self.env.uid).tz:
-            raise UserError(_('Please Set a User Timezone'))
-        if self.start_date and self.end_date:
-            start = str(self.start_date).split('-')
-            end = str(self.end_date).split('-')
-            if not start[1] == end[1]:
-                raise UserError(_('The Dates Can of Same Month Only'))
+        # for user in users:
+        #     if not user.labour_card_number:
+        #         flags['labour_card_number'] = False
+        #     if not user.salary_card_number:
+        #         flags['salary_card_number'] = False
+        #     if not user.agent_id:
+        #         flags['agent_id'] = False
+        # if not flags['labour_card_number']:
+        #     raise UserError(_('Please Set Labour Card Number of All Employees'))
+        # if not flags['salary_card_number']:
+        #     raise UserError(_('Please Set Salary Card Number / Account Number '
+        #                       'of All Employees'))
+        # if not flags['agent_id']:
+        #     raise UserError(
+        #         _('Please Set Employee Card Number of All Employees'))
+        # if not self.env['res.users'].browse(self.env.uid).tz:
+        #     raise UserError(_('Please Set a User Timezone'))
+        # if self.start_date and self.end_date:
+        #     start = str(self.start_date).split('-')
+        #     end = str(self.end_date).split('-')
+        #     if not start[1] == end[1]:
+        #         raise UserError(_('The Dates Can of Same Month Only'))
         slips = self.env['wps.wizard'].get_data(self.start_date, self.end_date)
-        if not slips:
-            raise UserError(_('There are no payslip Created for the selected '
-                              'month'))
+        # if not slips:
+        #     raise UserError(_('There are no payslip Created for the selected '
+        #                       'month'))
         company = self.env['res.company']._company_default_get('wps.wizard')
         user = self.env['res.users'].browse(self.env.uid)
         if user.tz:
