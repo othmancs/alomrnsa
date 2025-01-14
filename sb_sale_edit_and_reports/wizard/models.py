ee721bf7f84e1486293a02059b37b033431cf5c4
othmancs <61784613+othmancs@users.noreply.github.com>
2025-01-14 11:27:23 +0300

    Update models.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/wizard/models.py
+++ sb_sale_edit_and_reports/wizard/models.py
@@ -10,12 +10,12 @@ class SalesReportWizard(models.TransientModel):
     company_id = fields.Many2one('res.company', required=True, readonly=True, default=lambda self: self.env.company.id)
     printed_by = fields.Char(string="طبع بواسطة", compute="_compute_printed_by")
     print_date = fields.Date(string="تاريخ الطباعة", default=fields.Date.context_today)
-payment_method_line_id = fields.Many2one(
-    'account.payment.method.line',
-    string="طريقة الدفع",
-    default=lambda self: self.env['account.payment.method.line'].search([], limit=1),
-    domain="[('company_id', '=', company_id)]"
-)
+# payment_method_line_id = fields.Many2one(
+#     'account.payment.method.line',
+#     string="طريقة الدفع",
+#     default=lambda self: self.env['account.payment.method.line'].search([], limit=1),
+#     domain="[('company_id', '=', company_id)]"
+# )
 
     def _compute_printed_by(self):
         for record in self:
