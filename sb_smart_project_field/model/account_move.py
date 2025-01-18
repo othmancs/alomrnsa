2bdf3b8318fa0616ffdf36d7224bfccf8755dc59
othmancs <61784613+othmancs@users.noreply.github.com>
2025-01-18 08:58:17 +0300

    Update account_move.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_smart_project_field/model/account_move.py
+++ sb_smart_project_field/model/account_move.py
@@ -8,18 +8,18 @@ class AccountInvoice(models.Model):
     smart_field=fields.Char(string="الرقم المرجعى لبرناج سمارت")
 
 
-    def action_post(self):
-        for invoice in self:
-            # Check if the required field is empty or has less than 4 digits
-            if not invoice.smart_field:
-                raise UserError("خطا مستخدم")
-            if len(str(invoice.smart_field)) < 4:
-                raise UserError("خطا مستخدم")
-                # Check if the field contains only zeros
-            if str(invoice.smart_field).strip('0') == '':
-                raise UserError("خطا مستخدم")
-                # Check if the field starts with zero
-            if str(invoice.smart_field).startswith('0'):
-                raise UserError("خطا مستخدم")
-        # Call the original method to proceed with posting
-        return super(AccountInvoice, self).action_post()
+    # def action_post(self):
+    #     for invoice in self:
+    #         # Check if the required field is empty or has less than 4 digits
+    #         if not invoice.smart_field:
+    #             raise UserError("خطا مستخدم")
+    #         if len(str(invoice.smart_field)) < 4:
+    #             raise UserError("خطا مستخدم")
+    #             # Check if the field contains only zeros
+    #         if str(invoice.smart_field).strip('0') == '':
+    #             raise UserError("خطا مستخدم")
+    #             # Check if the field starts with zero
+    #         if str(invoice.smart_field).startswith('0'):
+    #             raise UserError("خطا مستخدم")
+    #     # Call the original method to proceed with posting
+    #     return super(AccountInvoice, self).action_post()
