d1743b40a49ac9dd19b65c21303eca6c4ff1d030
othmancs <61784613+othmancs@users.noreply.github.com>
2025-01-21 17:31:14 +0300

    Update branch_stock_picking.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- multi_branch_base/models/branch_stock_picking.py
+++ multi_branch_base/models/branch_stock_picking.py
@@ -20,6 +20,48 @@
 #
 #############################################################################
 
+# from odoo import models, fields, api
+
+
+# class StockPicking(models.Model):
+#     """inherited stock.picking"""
+#     _inherit = "stock.picking"
+
+#     def _get_default_branch_id(self):
+#         if len(self.env.user.branch_ids) == 1:
+#             sp_company = self.company_id if self.company_id else self.env.company
+#             branch_ids = self.env.user.branch_ids
+#             branch = branch_ids.filtered(
+#                 lambda branch: branch.company_id == sp_company)
+#             if branch:
+#                 return branch
+#             else:
+#                 return False
+#         return False
+
+#     branch_id = fields.Many2one("res.branch", string='Branch',
+#                                 readonly=False, store=True,
+#                                 compute="_compute_branch_id",
+#                                 default=_get_default_branch_id)
+
+#     @api.depends('sale_id', 'purchase_id')
+#     def _compute_branch_id(self):
+#         """methode to compute branch"""
+#         for record in self:
+#             record.branch_id = False
+#             if record.sale_id.branch_id:
+#                 record.branch_id = record.sale_id.branch_id
+#             if record.purchase_id.branch_id:
+#                 record.branch_id = record.purchase_id.branch_id
+
+
+# class StockPickingTypes(models.Model):
+#     """inherited stock picking type"""
+#     _inherit = "stock.picking.type"
+
+#     branch_id = fields.Many2one('res.branch', string='Branch', store=True,
+#                                 related='warehouse_id.branch_id')
+
 from odoo import models, fields, api
 
 
@@ -28,36 +70,46 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def _get_default_branch_id(self):
-        if len(self.env.user.branch_ids) == 1:
+        """تحديد الفرع الافتراضي بدون قيود الصلاحيات"""
+        if len(self.env.user.branch_ids.sudo()) == 1:
             sp_company = self.company_id if self.company_id else self.env.company
-            branch_ids = self.env.user.branch_ids
+            branch_ids = self.env.user.branch_ids.sudo()
             branch = branch_ids.filtered(
                 lambda branch: branch.company_id == sp_company)
             if branch:
                 return branch
-            else:
-                return False
         return False
 
-    branch_id = fields.Many2one("res.branch", string='Branch',
-                                readonly=False, store=True,
-                                compute="_compute_branch_id",
-                                default=_get_default_branch_id)
+    branch_id = fields.Many2one(
+        "res.branch",
+        string='Branch',
+        readonly=False,
+        store=True,
+        compute="_compute_branch_id",
+        default=_get_default_branch_id,
+    )
 
     @api.depends('sale_id', 'purchase_id')
     def _compute_branch_id(self):
-        """methode to compute branch"""
+        """تعيين الفرع بدون قيود الصلاحيات"""
         for record in self:
             record.branch_id = False
             if record.sale_id.branch_id:
-                record.branch_id = record.sale_id.branch_id
+                record.branch_id = record.sale_id.branch_id.sudo()
             if record.purchase_id.branch_id:
-                record.branch_id = record.purchase_id.branch_id
+                record.branch_id = record.purchase_id.branch_id.sudo()
 
 
 class StockPickingTypes(models.Model):
     """inherited stock picking type"""
     _inherit = "stock.picking.type"
 
-    branch_id = fields.Many2one('res.branch', string='Branch', store=True,
-                                related='warehouse_id.branch_id')
+    branch_id = fields.Many2one(
+        'res.branch',
+        string='Branch',
+        store=True,
+        related='warehouse_id.branch_id',
+        readonly=False,
+    )
+
+
