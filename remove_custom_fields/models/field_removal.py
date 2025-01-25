77b397f5cd1a1c031f18ff2efd12e1f73d18f539
othmancs <61784613+othmancs@users.noreply.github.com>
2025-01-25 21:51:31 +0300

    Update field_removal.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- remove_custom_fields/models/field_removal.py
+++ remove_custom_fields/models/field_removal.py
@@ -3,7 +3,7 @@ from odoo import api, SUPERUSER_ID
 def remove_custom_fields(cr, registry):
     """
     This method removes the fields 'pricelist_item_id' and 'x_product_pricelist_item_id'
-    from the database.
+    from the product.pricelist.item model.
     """
     env = api.Environment(cr, SUPERUSER_ID, {})
     
@@ -14,7 +14,10 @@ def remove_custom_fields(cr, registry):
     ]
     
     for field_name in fields_to_remove:
-        field = env['ir.model.fields'].search([('name', '=', field_name)])
+        field = env['ir.model.fields'].search([
+            ('name', '=', field_name),
+            ('model', '=', 'product.pricelist.item')  # التأكد من أن الحقل ينتمي لهذا النموذج
+        ])
         if field:
-            print(f"Removing field: {field_name}")
+            print(f"Removing field: {field_name} from model product.pricelist.item")
             field.unlink()
