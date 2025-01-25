a289a58653717136edd65be399952e7234df7178
othmancs <61784613+othmancs@users.noreply.github.com>
2025-01-25 22:36:58 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- custom_sale_order_fields/models/sale_order.py
+++ custom_sale_order_fields/models/sale_order.py
@@ -1,7 +1,19 @@
 
+# from odoo import models, fields, api
+
+# class SaleOrderLine(models.Model):
+#     _inherit = 'sale.order.line'
+
+#     pricelist_item_id = fields.Many2one('product.pricelist.item', store=True)  # جعل الحقل مخزنًا
 from odoo import models, fields, api
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
+    # إزالة الحقل غير المخزن وإنشاء حقل جديد مخزن
     pricelist_item_id = fields.Many2one('product.pricelist.item', store=True)  # جعل الحقل مخزنًا
+
+    @api.model
+    def _remove_old_field(self):
+        # تنفيذ أوامر لإزالة الحقول القديمة من النموذج إذا لزم الأمر
+        self.env.cr.execute('''ALTER TABLE sale_order_line DROP COLUMN IF EXISTS pricelist_item_id''')
