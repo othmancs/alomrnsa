e1e97780e72bd3028e0d06c8003cf124cab1662d
othmancs <61784613+othmancs@users.noreply.github.com>
2025-01-26 02:08:29 +0300

    Update sale_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- custom_sale_order_fields/models/sale_order.py
+++ custom_sale_order_fields/models/sale_order.py
@@ -1,26 +1,19 @@
-
-# from odoo import models, fields, api
-
-# class SaleOrderLine(models.Model):
-#     _inherit = 'sale.order.line'
-
-#     pricelist_item_id = fields.Many2one('product.pricelist.item', store=True)  # جعل الحقل مخزنًا
 from odoo import models, fields, api
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    # إزالة الحقل غير المخزن وإنشاء حقل جديد مخزن
-    # pricelist_item_id = fields.Many2one('product.pricelist.item', store=True)  # جعل الحقل مخزنًا
-    # pricelist_item_id = fields.Many2one(
-    #     'product.pricelist.item',
-    #     string="Pricelist Item",
-    #     required=True,  # الحقل مطلوب
-    #     ondelete='restrict'  # يمكن استخدام "restrict" أو "cascade"
-    # )
-   pricelist_item_id = fields.Many2one('product.pricelist.item', string="Pricelist Item", store=True)
+   pricelist_item_id = fields.Many2one(
+        'product.pricelist.item',
+        string='Pricelist Item',
+        compute='_compute_pricelist_item_id',
+        store=True  # تخزين الحقل في قاعدة البيانات
+    )
 
-    @api.model
-    def _remove_old_field(self):
-        # تنفيذ أوامر لإزالة الحقول القديمة من النموذج إذا لزم الأمر
-        self.env.cr.execute('''ALTER TABLE sale_order_line DROP COLUMN IF EXISTS pricelist_item_id''')
+    @api.depends('product_id', 'order_id.pricelist_id')
+    def _compute_pricelist_item_id(self):
+        for line in self:
+            line.pricelist_item_id = self.env['product.pricelist.item'].search([
+                ('product_id', '=', line.product_id.id),
+                ('pricelist_id', '=', line.order_id.pricelist_id.id)
+            ], limit=1)
