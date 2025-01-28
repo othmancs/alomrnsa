2e975638a14cd59b543f4a0ea6afb42401781aa5
othmancs <61784613+othmancs@users.noreply.github.com>
2025-01-28 13:19:23 +0300

    Update sale_order_inherit.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- bi_sale_pricelist/models/sale_order_inherit.py
+++ bi_sale_pricelist/models/sale_order_inherit.py
@@ -5,7 +5,11 @@ from odoo import api, fields, models, _
     
 class SaleOrderLine(models.Model):
     _inherit="sale.order.line"
-    
+    pricelist_item_id = fields.Many2one(
+        'product.pricelist.item',
+        string="عنصر قائمة الأسعار",
+        store=True  # إضافة هذا لجعل الحقل مخزن
+    )
     def pricelist_apply(self):
         return {
                 'view_type': 'form',
