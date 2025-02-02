a898d76997c2f0e396f98f8d4ce68eae0da511c4
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-02 14:16:50 +0300

    Update account_move.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/account_move.py
+++ sb_sale_edit_and_reports/model/account_move.py
@@ -4,10 +4,10 @@ from odoo import fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    payment_method = fields.Selection([
-        ('option1', 'نقدى'),
-        ('option2', 'اجل'),
-    ], string='طريقه الدفع' ,readonly=True)
+    # payment_method = fields.Selection([
+    #     ('option1', 'نقدى'),
+    #     ('option2', 'اجل'),
+    # ], string='طريقه الدفع' ,readonly=True)
       payment_type = fields.Selection([  # تم تغيير الحقل إلى Selection
             ('cash', 'كاش'),
             ('credit', 'آجل')
