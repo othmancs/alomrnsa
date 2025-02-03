d5dc010e7984410fef61895702d8477ddc65b5e1
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-03 23:36:47 +0300

    Update stock_picking.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- invoice_delivery_action/models/stock_picking.py
+++ invoice_delivery_action/models/stock_picking.py
@@ -4,16 +4,13 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     def button_validate(self):
-        for picking in self:
-            sale_order = self.env['sale.order'].search([('name', '=', picking.origin)], limit=1)
-            if sale_order:
-                invoices = self.env['account.move'].search([('invoice_origin', '=', sale_order.name), ('state', '!=', 'cancel')])
-                if not invoices:
-                    raise exceptions.UserError("لا يمكنك تصديق أمر التسليم قبل إنشاء الفاتورة المرتبطة بأمر البيع.")
-        
+        """
+        تعديل وظيفة تصديق أمر التسليم (Stock Picking) للسماح بإنشاء الفاتورة قبل تصديق الكميات.
+        """
         # تجاوز قيود Odoo والسماح بالتصديق حتى بدون كميات محجوزة
         if self.state == 'assigned' and not any(move.quantity_done for move in self.move_lines):
             for move in self.move_lines:
                 move.quantity_done = move.product_uom_qty  # ضبط الكمية المنجزة تلقائيًا
         
+        # السماح بالتصديق دون التحقق من وجود فاتورة
         return super(StockPicking, self).button_validate()
