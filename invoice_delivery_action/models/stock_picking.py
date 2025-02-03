f1f3dd841f410f1f9bb8a3002ce5b5743450e996
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-03 23:43:09 +0300

    Update stock_picking.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- invoice_delivery_action/models/stock_picking.py
+++ invoice_delivery_action/models/stock_picking.py
@@ -1,4 +1,4 @@
-from odoo import models, api, exceptions
+from odoo import models, exceptions
 
 class StockPicking(models.Model):
     _inherit = "stock.picking"
@@ -8,8 +8,8 @@ class StockPicking(models.Model):
         تعديل وظيفة تصديق أمر التسليم (Stock Picking) للسماح بإنشاء الفاتورة قبل تصديق الكميات.
         """
         # تجاوز قيود Odoo والسماح بالتصديق حتى بدون كميات محجوزة
-        if self.state == 'assigned' and not any(move.quantity_done for move in self.move_lines):
-            for move in self.move_lines:
+        if self.state == 'assigned' and not any(move.quantity_done for move in self.move_ids):
+            for move in self.move_ids:
                 move.quantity_done = move.product_uom_qty  # ضبط الكمية المنجزة تلقائيًا
         
         # السماح بالتصديق دون التحقق من وجود فاتورة
