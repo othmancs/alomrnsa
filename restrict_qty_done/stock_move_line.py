01db6980d1983585cf9f6e188a280cce34a7b4dd
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-03 23:30:20 +0300

    Update stock_move_line.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_qty_done/stock_move_line.py
+++ restrict_qty_done/stock_move_line.py
@@ -7,5 +7,13 @@ class StockMoveLine(models.Model):
     @api.constrains('qty_done')
     def _check_qty_done(self):
         for record in self:
-            if record.qty_done > record.product_uom_qty:
+            if record.qty_done > record.reserved_uom_qty:
+                raise ValidationError("الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المحجوزة.")
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    @api.constrains('quantity_done')
+    def _check_quantity_done(self):
+        for record in self:
+            if record.quantity_done > record.product_uom_qty:
                 raise ValidationError("الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المحجوزة.")
