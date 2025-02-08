869b5b17ad79f2eaf2a8e03b70bb55b95ae0a421
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-08 15:39:53 +0300

    Update __manifest__.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- prevent_zero_payment/__manifest__.py
+++ prevent_zero_payment/__manifest__.py
@@ -4,9 +4,11 @@
     "summary": "منع إنشاء دفعات بقيمة صفر في Odoo",
     "category": "Accounting",
     "author": "Essam Al Mahi",
+    "website": "https://www.example.com",
     "license": "LGPL-3",
     "depends": ["account"],
     "data": [],
     "installable": True,
     "application": False,
+    "auto_install": False
 }
