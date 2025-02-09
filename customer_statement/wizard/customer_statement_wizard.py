5a7a64fedc550e09dc8efe2ed8a57e1511a1eaeb
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-09 22:45:10 +0300

    Update customer_statement_wizard.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customer_statement/wizard/customer_statement_wizard.py
+++ customer_statement/wizard/customer_statement_wizard.py
@@ -4,14 +4,14 @@ class CustomerStatementWizard(models.TransientModel):
     _name = 'customer.statement.wizard'
     _description = 'Customer Statement Wizard'
 
-    start_date = fields.Date(string='Start Date', required=True)
-    end_date = fields.Date(string='End Date', required=True)
+    date_from = fields.Date(string='Start Date', required=True)
+    date_to = fields.Date(string='End Date', required=True)
     partner_id = fields.Many2one('res.partner', string='Customer', required=True)
 
     def generate_report(self):
         data = {
-            'start_date': self.start_date,
-            'end_date': self.end_date,
+            'date_from': self.date_from,
+            'date_to': self.date_to,
             'partner_id': self.partner_id.id,
         }
         return self.env.ref('customer_statement.action_report_customer_statement').report_action(None, data=data)
