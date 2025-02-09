6874d3c053434700e7058ee5dfa975c1924d3466
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-10 00:43:41 +0300

    Update report_partner_ledger.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- accounting_pdf_reports/report/report_partner_ledger.py
+++ accounting_pdf_reports/report/report_partner_ledger.py
@@ -3,7 +3,9 @@
 import time
 from odoo import api, models, _
 from odoo.exceptions import UserError
+import logging
 
+_logger = logging.getLogger(__name__)
 
 class ReportPartnerLedger(models.AbstractModel):
     _name = 'report.accounting_pdf_reports.report_partnerledger'
@@ -13,10 +15,13 @@ class ReportPartnerLedger(models.AbstractModel):
         full_account = []
         currency = self.env['res.currency']
         query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
-        reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".full_reconcile_id IS NULL '
+        reconcile_clause = "" if data['form'].get('reconciled', False) else ' AND "account_move_line".full_reconcile_id IS NULL '
+        
+        if not data.get('computed') or not data['computed'].get('move_state') or not data['computed'].get('account_ids'):
+            raise UserError(_('Missing computed data for the report.'))
+        
         params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
         
-        # استعلام لجلب الرصيد الافتتاحي قبل التاريخ المحدد
         query_opening_balance = """
             SELECT COALESCE(SUM("account_move_line".debit - "account_move_line".credit), 0)
             FROM """ + query_get_data[0] + """
@@ -29,7 +34,7 @@ class ReportPartnerLedger(models.AbstractModel):
         self.env.cr.execute(query_opening_balance, (partner.id, tuple(data['computed']['account_ids']), data['form']['date_from'], tuple(data['computed']['move_state'])))
         opening_balance = self.env.cr.fetchone()[0] or 0.0
         
-        sum = opening_balance  # بدء التراكمي من الرصيد الافتتاحي
+        sum = opening_balance
         
         query = """
             SELECT "account_move_line".id, "account_move_line".date, j.code, acc.code as a_code, 
@@ -46,11 +51,10 @@ class ReportPartnerLedger(models.AbstractModel):
                 AND m.state IN %s
                 AND "account_move_line".account_id IN %s AND """ + query_get_data[1] + reconcile_clause + """
             ORDER BY "account_move_line".date"""
-
+        
         self.env.cr.execute(query, tuple(params))
         res = self.env.cr.dictfetchall()
         
-        # إضافة الرصيد الافتتاحي إذا لم تكن هناك حركات
         if not res:
             full_account.append({
                 'date': data['form']['date_from'],
@@ -62,7 +66,6 @@ class ReportPartnerLedger(models.AbstractModel):
             })
         
         for r in res:
-            r['date'] = r['date']
             r['displayed_name'] = '-'.join(
                 r[field_name] for field_name in ('move_name', 'ref', 'name')
                 if r[field_name] not in (None, '', '/')
@@ -74,39 +77,15 @@ class ReportPartnerLedger(models.AbstractModel):
         
         return full_account
 
-    def _sum_partner(self, data, partner, field):
-        if field not in ['debit', 'credit', 'debit - credit']:
-            return 0.0
-        result = 0.0
-        query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
-        reconcile_clause = "" if data['form']['reconciled'] else ' AND "account_move_line".full_reconcile_id IS NULL '
-
-        params = [partner.id, tuple(data['computed']['move_state']), tuple(data['computed']['account_ids'])] + query_get_data[2]
-        query = """SELECT sum(""" + field + """)
-                FROM """ + query_get_data[0] + """, account_move AS m
-                WHERE "account_move_line".partner_id = %s
-                    AND m.id = "account_move_line".move_id
-                    AND m.state IN %s
-                    AND account_id IN %s
-                    AND """ + query_get_data[1] + reconcile_clause
-        self.env.cr.execute(query, tuple(params))
-
-        contemp = self.env.cr.fetchone()
-        if contemp is not None:
-            result = contemp[0] or 0.0
-        return result
-
     @api.model
     def _get_report_values(self, docids, data=None):
-        if not data.get('form'):
-            raise UserError(_("Form content is missing, this report cannot be printed."))
+        if not data or not data.get('form'):
+            raise UserError(_('Form content is missing, this report cannot be printed.'))
+        
         data['computed'] = {}
-
         obj_partner = self.env['res.partner']
         query_get_data = self.env['account.move.line'].with_context(data['form'].get('used_context', {}))._query_get()
-        data['computed']['move_state'] = ['draft', 'posted']
-        if data['form'].get('target_move', 'all') == 'posted':
-            data['computed']['move_state'] = ['posted']
+        data['computed']['move_state'] = ['draft', 'posted'] if data['form'].get('target_move', 'all') == 'all' else ['posted']
         
         self.env.cr.execute("""
             SELECT a.id
@@ -114,10 +93,10 @@ class ReportPartnerLedger(models.AbstractModel):
             WHERE a.account_type IN %s
             AND NOT a.deprecated""", (tuple(['asset_receivable', 'liability_payable']),))
         data['computed']['account_ids'] = [a for (a,) in self.env.cr.fetchall()]
-
+        
         self.env.cr.execute("""
             SELECT DISTINCT "account_move_line".partner_id
-            FROM """ + query_get_data[0] + """, account_move AS am
+            FROM """ + query_get_data[0] + ", account_move AS am
             WHERE "account_move_line".partner_id IS NOT NULL
                 AND am.id = "account_move_line".move_id
                 AND am.state IN %s
@@ -126,7 +105,7 @@ class ReportPartnerLedger(models.AbstractModel):
         
         partner_ids = [res['partner_id'] for res in self.env.cr.dictfetchall()]
         partners = obj_partner.browse(partner_ids)
-
+        
         return {
             'doc_ids': partner_ids,
             'doc_model': 'res.partner',
@@ -134,8 +113,8 @@ class ReportPartnerLedger(models.AbstractModel):
             'docs': partners,
             'time': time,
             'lines': self._lines,
-            'sum_partner': self._sum_partner,
         }
+
 
 
 
