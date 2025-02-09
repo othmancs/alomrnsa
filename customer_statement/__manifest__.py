4eb38fcac5673bddfe4a631744ec03e0a1b496c5
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-09 22:33:46 +0300

    Update __manifest__.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customer_statement/__manifest__.py
+++ customer_statement/__manifest__.py
@@ -6,7 +6,7 @@
     'author': 'Essam Al Mahi',
     'depends': ['account'],
     'data': [
-        'security/ir.model.access.csv',
+        # 'security/ir.model.access.csv',
         'views/customer_statement_views.xml',
         'wizard/customer_statement_wizard.xml',
         'reports/customer_statement_report.xml',
