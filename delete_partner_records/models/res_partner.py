5158331e172fd6de640f85bec52fbe5abd3d0e4f
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-11 11:21:13 +0300

    Update res_partner.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- delete_partner_records/models/res_partner.py
+++ delete_partner_records/models/res_partner.py
@@ -2,7 +2,20 @@ from odoo import models, api
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
+    
+   @api.multi
+   def delete_with_related_moves(self):
+        for partner in self:
+            # البحث عن الحركات المرتبطة بالشريك
+            moves = self.env['account.move'].search([('partner_id', '=', partner.id)])
+            if moves:
+                # حذف الحركات المرتبطة
+                moves.unlink()
 
+            # حذف الشريك نفسه
+            partner.unlink()
+
+        return True
     def unlink(self):
         for partner in self:
             models_to_delete = [
@@ -16,22 +29,3 @@ class ResPartner(models.Model):
             for model in models_to_delete:
                 self.env[model].search([('partner_id', '=', partner.id)]).unlink()
         
-        return super(ResPartner, self).unlink()
-        from odoo import models, fields, api
-
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-
-    @api.multi
-    def delete_with_related_moves(self):
-        for partner in self:
-            # البحث عن الحركات المرتبطة بالشريك
-            moves = self.env['account.move'].search([('partner_id', '=', partner.id)])
-            if moves:
-                # حذف الحركات المرتبطة
-                moves.unlink()
-
-            # حذف الشريك نفسه
-            partner.unlink()
-
-        return True
