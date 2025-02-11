526412e012425486c3ac2ac27b2e3da8c65eabca
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-11 11:20:25 +0300

    Update res_partner.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- delete_partner_records/models/res_partner.py
+++ delete_partner_records/models/res_partner.py
@@ -17,3 +17,21 @@ class ResPartner(models.Model):
                 self.env[model].search([('partner_id', '=', partner.id)]).unlink()
         
         return super(ResPartner, self).unlink()
+        from odoo import models, fields, api
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    @api.multi
+    def delete_with_related_moves(self):
+        for partner in self:
+            # البحث عن الحركات المرتبطة بالشريك
+            moves = self.env['account.move'].search([('partner_id', '=', partner.id)])
+            if moves:
+                # حذف الحركات المرتبطة
+                moves.unlink()
+
+            # حذف الشريك نفسه
+            partner.unlink()
+
+        return True
