97ac8be2869e755210fe3564f4f0757238f7f6dc
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-13 10:52:12 +0300

    Update stock_picking.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_seller_field/model/stock_picking.py
+++ sb_seller_field/model/stock_picking.py
@@ -1,8 +1,23 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class StockPicking(models.Model):
     _inherit = 'stock.picking'
 
-    created_by_id = fields.Many2one('res.partner', string='انشأ من قبل',domain="[('branch_id', '=', branch_id)]")
+    created_by_id = fields.Many2one(
+        'res.partner', 
+        string='أنشئ من قبل', 
+        domain="[('branch_id', '=', branch_id)]"
+    )
 
+    @api.depends('sale_id')
+    def _compute_created_by_id(self):
+        for picking in self:
+            picking.created_by_id = picking.sale_id.created_by_id if picking.sale_id else False
+
+    created_by_id = fields.Many2one(
+        'res.partner', 
+        string='أنشئ من قبل', 
+        compute='_compute_created_by_id', 
+        store=True
+    )
