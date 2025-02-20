6b4795b5117ef25d24385d70a2d133273493e2f9
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-20 09:53:46 +0300

    Update sales_report.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_sale_edit_and_reports/model/sales_report.py
+++ sb_sale_edit_and_reports/model/sales_report.py
@@ -13,35 +13,52 @@ class SalesReportReport(models.AbstractModel):
             row = 0
             col = 0
             # تعديل حجم الأعمدة
-            worksheet.set_column(0, 0, 20)  # للعمود الأول
-            worksheet.set_column(1, 1, 30)  # للعمود الثاني
-            worksheet.set_column(2, 2, 20)  # للعمود الثالث
-            worksheet.set_column(3, 3, 15)  # للعمود الرابع
-            worksheet.set_column(4, 4, 12)  # للعمود الخامس
-            worksheet.set_column(5, 5, 12)  # للعمود السادس
-            worksheet.set_column(6, 6, 12)  # للعمود السابع
-            worksheet.set_column(7, 7, 12)  # للعمود الثامن
+            worksheet.set_column(0, 0, 20)
+            worksheet.set_column(1, 1, 30)
+            worksheet.set_column(2, 2, 20)
+            worksheet.set_column(3, 3, 15)
+            worksheet.set_column(4, 4, 12)
+            worksheet.set_column(5, 5, 12)
+            worksheet.set_column(6, 6, 12)
+            worksheet.set_column(7, 7, 12)
 
-            # إنشاء التنسيقات مع تعطيل التفاف النص
-            format1 = workbook.add_format({'text_wrap': False, 'font_size': 11, 'align': 'center', 'bold': True, 'border': 1, 'bg_color': '#CCC7BF'})
-            format2 = workbook.add_format({'text_wrap': False, 'font_size': 11, 'align': 'center', 'bold': False, 'border': 1})
-            format3 = workbook.add_format({'text_wrap': False, 'font_size': 10, 'border': 1, 'bold': False, 'align': 'center'})
-            format4 = workbook.add_format({'text_wrap': False, 'font_size': 12, 'align': 'center', 'bold': True})
-            format5 = workbook.add_format({'text_wrap': False, 'font_size': 12, 'align': 'center', 'bold': True, 'bg_color': '#caf0f8'})
-            format6 = workbook.add_format({'text_wrap': False, 'font_size': 12, 'align': 'center', 'bold': True, 'bg_color': 'red', 'color': 'white'})
-            format7 = workbook.add_format({'text_wrap': False, 'font_size': 12, 'align': 'center', 'bold': True, 'bg_color': 'green', 'color': 'white'})
+            # إنشاء التنسيقات
+            format1 = workbook.add_format({
+                'text_wrap': False, 'font_size': 11, 'align': 'center',
+                'bold': True, 'border': 1, 'bg_color': '#CCC7BF'
+            })
+            format2 = workbook.add_format({
+                'text_wrap': False, 'font_size': 11, 'align': 'center',
+                'border': 1
+            })
+            format3 = workbook.add_format({
+                'text_wrap': False, 'font_size': 10, 'border': 1,
+                'align': 'center'
+            })
+            format4 = workbook.add_format({
+                'text_wrap': False, 'font_size': 12, 'align': 'center',
+                'bold': True
+            })
+            format5 = workbook.add_format({
+                'text_wrap': False, 'font_size': 12, 'align': 'center',
+                'bold': True, 'bg_color': '#caf0f8'
+            })
+            format6 = workbook.add_format({
+                'text_wrap': False, 'font_size': 12, 'align': 'center',
+                'bold': True, 'bg_color': 'red', 'color': 'white'
+            })
+            format7 = workbook.add_format({
+                'text_wrap': False, 'font_size': 12, 'align': 'center',
+                'bold': True, 'bg_color': 'green', 'color': 'white'
+            })
 
-            domain = [('invoice_date', '>=', obj.date_start),
-                      ('invoice_date', '<=', obj.date_end),
-                      ('move_type', '=', 'out_invoice'),
-                      ('state', '=', 'posted')]
-
-            # if isinstance(obj.payment_type, models.BaseModel):
-            #     domain.append(('payment_type', '=', obj.payment_type.id))
-            # else:
-            #     # إذا كانت payment_type نصًا أو فارغة، يمكنك إيقاف إضافة الفلتر
-            #     if obj.payment_type:
-            #         domain.append(('payment_type', '=', obj.payment_type))
+            # تحديد الشروط باستخدام التواريخ وطريقة الدفع والفروع
+            domain = [
+                ('invoice_date', '>=', obj.date_start),
+                ('invoice_date', '<=', obj.date_end),
+                ('move_type', '=', 'out_invoice'),
+                ('state', '=', 'posted')
+            ]
             if obj.payment_type:
                 domain.append(('payment_state', '=', obj.payment_type))
             if obj.branch_ids:
@@ -61,23 +78,20 @@ class SalesReportReport(models.AbstractModel):
             worksheet.write(row + 4, col + 1, self.env.user.name, format3)
 
             row += 8
-
             totals_by_payment_type = {}
 
             for branch in existing_branches:
                 current_branch_lines = lines_data.filtered(lambda x: x.branch_id == branch)
-                unit_price_branch = sum([sum(move.mapped('amount_untaxed')) for move in current_branch_lines.filtered(lambda x: x.move_type != 'out_refund')])
-                total_discount_branch = sum([sum(move.line_ids.mapped('discount')) for move in current_branch_lines.filtered(lambda x: x.move_type != 'out_refund')])
-                branch_amount_untaxed = sum([sum(move.line_ids.mapped(lambda line: (line.price_unit * line.quantity) - line.discount)) for move in current_branch_lines.filtered(lambda x: x.move_type != 'out_refund')])
-
+                unit_price_branch = sum([
+                    sum(move.mapped('amount_untaxed'))
+                    for move in current_branch_lines.filtered(lambda x: x.move_type != 'out_refund')
+                ])
+                # يمكن إضافة المزيد من الحسابات حسب الحاجة
                 worksheet.write(row, col, branch.name, format5)
                 worksheet.merge_range(row, col + 1, row, col + 4, '', format5)
                 worksheet.write(row, col + 5, unit_price_branch, format5)
 
-                # تعريف total_out_refund_price بشكل صحيح
-                total_out_refund_price = 0.0
                 total_out_refund_purchase_price = 0.0
-                
                 for line in current_branch_lines:
                     out_refund_price = self.env['account.move'].search([
                         ('move_type', '=', 'out_refund'),
@@ -85,12 +99,10 @@ class SalesReportReport(models.AbstractModel):
                         ('reversed_entry_id', '=', line.id),
                         ('state', '=', 'posted')
                     ])
-                    total_out_refund_price += sum(out_refund_price.line_ids.mapped(lambda x: x.price_unit * x.quantity))
-                    total_out_refund_purchase_price += sum(out_refund_price.line_ids.mapped(lambda x: x.purchase_price * x.quantity))
-                    
-                # worksheet.write(row, col + 6, total_out_refund_price, format5)
+                    total_out_refund_purchase_price += sum(
+                        out_refund_price.line_ids.mapped(lambda x: x.purchase_price * x.quantity)
+                    )
                 worksheet.write(row, col + 6, total_out_refund_purchase_price, format5)
-
                 row += 1
 
                 # كتابة العناوين
@@ -100,8 +112,8 @@ class SalesReportReport(models.AbstractModel):
                 worksheet.write(row, col + 3, 'طريقة الدفع  ', format1)
                 worksheet.write(row, col + 4, 'التاريخ  ', format1)
                 worksheet.write(row, col + 5, 'صافى البيع ق ', format1)
-                worksheet.write(row, col + 6, 'صافي الارجاعات ق ', format1)  # العمود الجديد
-                worksheet.write(row, col + 7, ' حاله الدفع ', format1)  # تعديل العمود لحالة الدفع
+                worksheet.write(row, col + 6, 'صافي الارجاعات ق ', format1)
+                worksheet.write(row, col + 7, ' حاله الدفع ', format1)
                 row += 1
 
                 for account in current_branch_lines:
@@ -114,15 +126,15 @@ class SalesReportReport(models.AbstractModel):
 
                     if payment_type not in totals_by_payment_type:
                         totals_by_payment_type[payment_type] = 0
-
                     totals_by_payment_type[payment_type] += sum(account.mapped('amount_untaxed'))
-
-                    net_cost = sum(account.line_ids.mapped(lambda line: (line.price_unit * line.quantity) - line.discount))
+                    net_cost = sum(
+                        account.line_ids.mapped(lambda line: (line.price_unit * line.quantity) - line.discount)
+                    )
 
                     if state == 'paid':
-                        worksheet.write(row, col + 7, 'مدفوع', format7)  # تعديل العمود لحالة الدفع
+                        worksheet.write(row, col + 7, 'مدفوع', format7)
                     elif state == 'not_paid':
-                        worksheet.write(row, col + 7, 'غير مدفوع', format6)  # تعديل العمود لحالة الدفع
+                        worksheet.write(row, col + 7, 'غير مدفوع', format6)
 
                     worksheet.write(row, col, invoice_number, format2)
                     worksheet.write(row, col + 1, seller_name, format2)
@@ -134,18 +146,18 @@ class SalesReportReport(models.AbstractModel):
                     else:
                         worksheet.write(row, col + 3, '-', format2)
                     worksheet.write(row, col + 4, format_date(self.env, invoice_date), format2)
-                    worksheet.write(row, col + 5, net_cost, format2)  # تم حذف العمود "إجمالي البيع"
-                   
-                    out_refund = self.env['account.move'].search([
-                            ('move_type', '=', 'out_refund'),
-                            ('branch_id', '=', branch.id),
-                            ('reversed_entry_id', '=', account.id),
-                            ('state', '=', 'posted')
-                        ])
+                    worksheet.write(row, col + 5, net_cost, format2)
 
+                    out_refund = self.env['account.move'].search([
+                        ('move_type', '=', 'out_refund'),
+                        ('branch_id', '=', branch.id),
+                        ('reversed_entry_id', '=', account.id),
+                        ('state', '=', 'posted')
+                    ])
                     for ac in out_refund:
-                        out_refund_purchase_price = sum(ac.line_ids.mapped(lambda x: x.purchase_price * x.quantity))
-                        out_refund_price = sum(ac.mapped('amount_untaxed'))
+                        out_refund_purchase_price = sum(
+                            ac.line_ids.mapped(lambda x: x.purchase_price * x.quantity)
+                        )
                         worksheet.write(row, col + 6, out_refund_purchase_price, format2)
                     row += 1
 
@@ -160,7 +172,6 @@ class SalesReportReport(models.AbstractModel):
                     payment_type_label = 'اجل'
                 else:
                     payment_type_label = 'غير محدد'
-                
                 worksheet.write(row, col, payment_type_label, format2)
                 worksheet.write(row, col + 1, total, format2)
                 row += 1
