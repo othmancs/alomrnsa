aca38f4598b460ab2ca5d03074fbd24ad43c4b13
othmancs <61784613+othmancs@users.noreply.github.com>
2025-02-25 18:27:12 +0300

    Update stock_move_line.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_qty_done/stock_move_line.py
+++ restrict_qty_done/stock_move_line.py
@@ -1,15 +1,15 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
-class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
+# class StockMoveLine(models.Model):
+#     _inherit = 'stock.move.line'
 
-    @api.constrains('qty_done')
-    def _check_qty_done(self):
-        for record in self:
-            if record.move_id.picking_id.picking_type_id.code == "outgoing":  # تحقق من كود العملية
-                if record.qty_done >= record.product_uom_qty:
-                    raise ValidationError("الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المحجوزة.")
+#     @api.constrains('qty_done')
+#     def _check_qty_done(self):
+#         for record in self:
+#             if record.move_id.picking_id.picking_type_id.code == "outgoing":  # تحقق من كود العملية
+#                 if record.qty_done >= record.product_uom_qty:
+#                     raise ValidationError("الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المحجوزة.")
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
