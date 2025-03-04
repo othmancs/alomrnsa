def61dd09db101e1080884ecf0fff7a44baf764d
othmancs <61784613+othmancs@users.noreply.github.com>
2025-03-04 11:33:00 +0300

    Add files via upload
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

---
+++ reports_designer_delivery_slip/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Delivery Slip (XLSX)',
+    'version': '1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Delivery Slip for Report Designer (XLSX, XLSM)',     
+    'price': 0.00,
+    'currency': 'EUR',
+    "license": "OPL-1",     
+    'description': """
+Delivery Slip.
+====================================
+Delivery Slip in MS Excel format (XLSX)
+Generate the Excel Report from a Template.
+Report for Report Designer (XLSX, XLSM). 
+    Odoo Report XLSX  Create Excel Report Excel Reports Accounting Reports Financial Report Financial Reports Stock Reports Inventory Reports \
+    Dynamic Sale Analysis Reports Export Excel Export xlsx Project Reports Warehouse Reports Purchases Reports Marketing Reports Sales Reports \
+    Report Designer Reports Designer Report Builder Reports Builder Product Report Customer Report POS Reports POS Report Analysis Report \
+    BI Report BI Reports BI Business Intelligence Report Business Intelligence Reports BI Analytics BI Analytic Data Analysis Reporting Tool
+    """,
+    'author': 'GTECH',
+    'support': 'vk.3141592653@gmail.com',
+    'depends': ['delivery', 'stock'],
+    'images': ['static/description/banner_rep.png'],
+    'data': [
+        'data/delivery_slip_xlsx.xml',
+    ],
+    'qweb': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    "pre_init_hook": "pre_init_check",
+}
