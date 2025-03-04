a78d73b9a2a1a2982304d055e76f5dcc1d2d5717
othmancs <61784613+othmancs@users.noreply.github.com>
2025-03-04 10:41:02 +0300

    Update stock_move_line.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_qty_done/stock_move_line.py
+++ restrict_qty_done/stock_move_line.py
@@ -1,15 +1,5 @@
-from odoo import models, fields, api
 from odoo.exceptions import ValidationError
-
-# class StockMoveLine(models.Model):
-#     _inherit = 'stock.move.line'
-
-#     @api.constrains('qty_done')
-#     def _check_qty_done(self):
-#         for record in self:
-#             if record.move_id.picking_id.picking_type_id.code == "outgoing":  # تحقق من كود العملية
-#                 if record.qty_done >= record.product_uom_qty:
-#                     raise ValidationError("الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المحجوزة.")
+from odoo import models, api
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
@@ -17,6 +7,6 @@ class StockMove(models.Model):
     @api.constrains('quantity_done')
     def _check_quantity_done(self):
         for record in self:
-            if record.picking_id.picking_type_id.code == "outgoing":  # تحقق من كود العملية
-                if record.quantity_done > record.product_uom_qty:
-                    raise ValidationError("الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المحجوزة.")
+            if record.picking_id.picking_type_id.code == "outgoing":  # تحقق من أن العملية "خروج"
+                if record.quantity_done > record.product_uom_qty:  # الكمية المنفذة لا تتجاوز الكمية المطلوبة
+                    raise ValidationError("⚠️ الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المطلوبة في عملية النقل!")
