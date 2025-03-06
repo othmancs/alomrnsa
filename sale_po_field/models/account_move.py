f1d36f444d2a9b952fdf4276366ac03008cd6d67
othmancs <61784613+othmancs@users.noreply.github.com>
2025-03-06 10:06:14 +0300

    Update account_move.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sale_po_field/models/account_move.py
+++ sale_po_field/models/account_move.py
@@ -3,4 +3,16 @@ from odoo import models, fields
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    purchase_order_number = fields.Char(string='رقم أمر الشراء', related='invoice_origin', readonly=True)
+    purchase_order_number = fields.Char(string='رقم أمر الشراء', readonly=True)
+
+    def _auto_init(self):
+        """ Ensure purchase_order_number is copied from sale order. """
+        res = super()._auto_init()
+        self.env.cr.execute("""
+            UPDATE account_move am
+            SET purchase_order_number = so.purchase_order_number
+            FROM sale_order so
+            WHERE am.invoice_origin = so.name
+            AND so.purchase_order_number IS NOT NULL
+        """)
+        return res
