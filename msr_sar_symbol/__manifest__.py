7050c32c85d624753c7441c3df63c48b3fef85cd
othmancs <61784613+othmancs@users.noreply.github.com>
2025-03-12 10:04:59 +0300

    Add files via upload
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

---
+++ msr_sar_symbol/__manifest__.py
@@ -0,0 +1,43 @@
+{
+    "name": "Saudi Riyal (SAR) Currency Symbol | SAR Symbol",
+    "version": "16.0.1.0.0",
+    "description": "Saudi Riyal (SAR) Currency New Symbol",
+    "summary": "SAR New Symbol",
+    "author": "MACHINSER",
+    "website": "https://machinser.com",
+    "license": "OPL-1",
+    "category": "Tools",
+    "depends": [
+        "base",
+        "web",
+    ],
+    "data": [
+        "data/res_currency_data.xml",
+    ],
+    "assets": {
+        "web.assets_backend": [
+            "msr_sar_symbol/static/src/css/style.css",
+        ],
+        "web.assets_frontend": [
+            "msr_sar_symbol/static/src/css/style.css",
+        ],
+        "web.assets_common": [
+            "msr_sar_symbol/static/src/css/style.css",
+        ],
+        "web.report_assets_common": [
+            "msr_sar_symbol/static/src/css/style.css",
+        ],
+        "web.report_assets_pdf": [
+            "msr_sar_symbol/static/src/css/style.css",
+        ],
+        "web.assets_qweb": [
+            "msr_sar_symbol/static/src/css/style.css",
+        ],
+        "point_of_sale._assets_pos": [
+            "msr_sar_symbol/static/src/css/style.css",
+        ],
+    },
+    "images": ["static/description/banner.gif"],
+    "auto_install": False,
+    "application": False,
+}
