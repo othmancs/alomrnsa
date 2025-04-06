39ed351b94cec3b3829290182f6910aef5fb4c7a
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-06 09:19:17 +0300

    Update __manifest__.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- partner_tax_merge/__manifest__.py
+++ partner_tax_merge/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    "name": "دمج جهات الاتصال والفواتير حسب الرقم الضريبي",
+    "name": "partner_tax_merge",
     "version": "16.0.1.0.0",
     "summary": "أداة لدمج جهات الاتصال والفواتير بناءً على الرقم الضريبي",
     "description": "هذا الموديول يمكنك من دمج جهات الاتصال التي لها نفس الرقم الضريبي مع تجاوز القيود الافتراضية مثل الحد الأقصى للدمج. كما يقوم بدمج الفواتير المرتبطة بنفس الرقم الضريبي.",
@@ -8,9 +8,9 @@
     "category": "Tools",
     "depends": ["base", "account"],
     "data": [
-        "wizard/merge.xml"
+        "wizard/merge.xml",
     ],
     "license": "LGPL-3",
-    "installable": true,
-    "application": false
+    "installable": True,
+    "application": False,
 }
