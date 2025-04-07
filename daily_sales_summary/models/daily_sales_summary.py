1e663a26a248bd46e044eef96323ae55d3eee5ff
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-08 01:22:10 +0300

    Update daily_sales_summary.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- daily_sales_summary/models/daily_sales_summary.py
+++ daily_sales_summary/models/daily_sales_summary.py
@@ -67,10 +67,10 @@ class DailySalesSummary(models.Model):
         currency_field='company_currency_id',
         compute='_compute_total_cash', store=True
     )
-    payment_method_totals = fields.Text(
+    payment_method_totals = fields.Html(
         string='المجاميع حسب طريقة الدفع',
         compute='_compute_payment_method_totals',
-        store=False
+        sanitize=False
     )
 
 
