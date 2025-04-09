21a9fc2234b434fea3f2149deae247d575ecd13f
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-10 00:58:42 +0300

    Update customer_statement.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customer_statement_OCS/models/customer_statement.py
+++ customer_statement_OCS/models/customer_statement.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
-
+import logging
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError
+
 _logger = logging.getLogger(__name__)
 import io
 import xlsxwriter
 import base64
 from datetime import datetime
-import logging
+
 
 
 class CustomerStatementReport(models.Model):
