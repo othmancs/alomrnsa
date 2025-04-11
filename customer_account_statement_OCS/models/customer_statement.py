ada28b905f82faafff109b723b8f9a4bc661b6a8
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-11 22:16:14 +0300

    Update customer_statement.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- customer_account_statement_OCS/models/customer_statement.py
+++ customer_account_statement_OCS/models/customer_statement.py
@@ -15,7 +15,6 @@ class CustomerStatementWizard(models.TransientModel):
     date_to = fields.Date(string='To Date', required=True, default=fields.Date.today)
     show_initial_balance = fields.Boolean(string='Show Initial Balance', default=True)
     show_invoice_details = fields.Boolean(string='Show Invoice Details', default=False)
-
     def action_print_statement(self):
         data = {
             'partner_id': self.partner_id.id,
@@ -24,4 +23,13 @@ class CustomerStatementWizard(models.TransientModel):
             'show_initial_balance': self.show_initial_balance,
             'show_invoice_details': self.show_invoice_details,
         }
-        return self.env.ref('customer_statement.action_customer_statement_report').report_action(self, data=data)
+        return self.env.ref('customer_account_statement_OCS.action_customer_statement_report').report_action(self, data=data)
+    # def action_print_statement(self):
+    #     data = {
+    #         'partner_id': self.partner_id.id,
+    #         'date_from': self.date_from,
+    #         'date_to': self.date_to,
+    #         'show_initial_balance': self.show_initial_balance,
+    #         'show_invoice_details': self.show_invoice_details,
+    #     }
+    #     return self.env.ref('customer_statement.action_customer_statement_report').report_action(self, data=data)
