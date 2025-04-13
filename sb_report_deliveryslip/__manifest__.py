7b5f55dfeffd3bddb4d7ae64214a36c286359ba0
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-13 12:13:46 +0300

    Update __manifest__.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- sb_report_deliveryslip/__manifest__.py
+++ sb_report_deliveryslip/__manifest__.py
@@ -2,9 +2,13 @@
 {
     'name': "sb_report_deliveryslip",
     'version': '0.1',
-    'depends': ['base', 'stock','sb_mr_driver_shipping_letter'],
+    'depends': ['base', 'stock', 'sb_mr_driver_shipping_letter'],
     'data': [
         'reports/sb_report_deliveryslip.xml',
-
     ],
+    'assets': {
+        'web.report_assets_common': [
+            'sb_report_deliveryslip/static/src/css/arabic_style.css',
+        ],
+    },
 }
