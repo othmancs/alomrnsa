17c244a81bf64bfaa05fde2a980e7c81ec1e340e
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-19 10:07:08 +0300

    Update daily_sales_summary.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- ‎daily_sales_summaryOCS/models/daily_sales_summary.py
+++ ‎daily_sales_summaryOCS/models/daily_sales_summary.py
@@ -395,7 +395,7 @@ class DailySalesSummary(models.Model):
         """إنشاء تقرير Excel للمبيعات والتحصيل حسب الهيكل المطلوب"""
         # إنشاء كتاب Excel
         output = io.BytesIO()
-        workbook = xlsxwriter.Workbook(output, {'in_memory': True,'right_to_left': True})
+        workbook = xlsxwriter.Workbook(output, {'in_memory': True,'right_to_left': False})
         worksheet = workbook.add_worksheet('المبيعات والتحصيل')
     
         # تنسيقات الخلايا
