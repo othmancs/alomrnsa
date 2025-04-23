bfad86311d5cc5aa265012a4e355258a26faa759
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-23 23:21:10 +0300

    Update purchase_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- stock_landed_costs_OCS/models/purchase_order.py
+++ stock_landed_costs_OCS/models/purchase_order.py
@@ -1,13 +1,18 @@
 # models/purchase_order.py
-
 from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
     
     landed_cost_total = fields.Float(
-        string='Landed Cost Total',
+        string='صافي التكلفة',
         compute='_compute_landed_cost_total',
+        store=True
+    )
+    
+    total_supplier_cost = fields.Float(
+        string='إجمالي المورد',
+        compute='_compute_total_supplier_cost',
         store=True
     )
     
@@ -15,10 +20,15 @@ class PurchaseOrder(models.Model):
     def _compute_landed_cost_total(self):
         for order in self:
             total = 0.0
-            if 'stock.landed.cost' in self.env:  # التحقق من وجود النموذج
+            if 'stock.landed.cost' in self.env:
                 for invoice in order.invoice_ids:
                     landed_costs = self.env['stock.landed.cost'].search([
                         ('vendor_bill_id', '=', invoice.id)
                     ])
                     total += sum(landed_costs.mapped('amount_total'))
             order.landed_cost_total = total
+    
+    @api.depends('amount_total', 'landed_cost_total')
+    def _compute_total_supplier_cost(self):
+        for order in self:
+            order.total_supplier_cost = order.amount_total + order.landed_cost_total
