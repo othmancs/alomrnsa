60f50e7268afbda5b60063f2c8f6b378b496a715
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-23 23:07:21 +0300

    Update purchase_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- stock_landed_costs_OCS/models/purchase_order.py
+++ stock_landed_costs_OCS/models/purchase_order.py
@@ -15,9 +15,10 @@ class PurchaseOrder(models.Model):
     def _compute_landed_cost_total(self):
         for order in self:
             total = 0.0
-            for invoice in order.invoice_ids:
-                landed_costs = self.env['stock.landed.costs'].search([
-                    ('vendor_bill_id', '=', invoice.id)
-                ])
-                total += sum(landed_costs.mapped('amount_total'))
+            if 'stock.landed.cost' in self.env:  # التحقق من وجود النموذج
+                for invoice in order.invoice_ids:
+                    landed_costs = self.env['stock.landed.cost'].search([
+                        ('vendor_bill_id', '=', invoice.id)
+                    ])
+                    total += sum(landed_costs.mapped('amount_total'))
             order.landed_cost_total = total
