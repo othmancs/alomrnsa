3b97e2f5943f5d1356b2c760c46154ae9946fd36
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-24 10:44:32 +0300

    Update purchase_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- stock_landed_costs_OCS/models/purchase_order.py
+++ stock_landed_costs_OCS/models/purchase_order.py
@@ -1,29 +1,36 @@
-# models/purchase_order.py
 from odoo import models, fields, api
 
 class PurchaseOrder(models.Model):
     _inherit = 'purchase.order'
-    
+
+    company_currency_id = fields.Many2one(
+        'res.currency',
+        string="Company Currency",
+        related='company_id.currency_id',
+        readonly=True
+    )
+
     landed_cost_total = fields.Float(
         string='صافي التكلفة',
         compute='_compute_landed_cost_total',
         store=True
     )
-    
+
+    total_in_sar = fields.Monetary(
+        string="Total in SAR",
+        compute='_compute_total_in_sar',
+        currency_field='company_currency_id',
+        readonly=True,
+        store=True
+    )
+
     total_supplier_cost = fields.Monetary(
         string="Total Supplier Cost",
         compute="_compute_total_supplier_cost",
         store=True,
         currency_field='currency_id'
     )
-    total_in_sar = fields.Monetary(
-    string="Total in SAR",
-    compute='_compute_total_in_sar',
-    currency_field='company_currency_id',
-    readonly=True,
-    store=True
-    )
-        
+
     @api.depends('invoice_ids')
     def _compute_landed_cost_total(self):
         for order in self:
@@ -35,13 +42,25 @@ class PurchaseOrder(models.Model):
                     ])
                     total += sum(landed_costs.mapped('amount_total'))
             order.landed_cost_total = total
+
     @api.depends('amount_total', 'landed_cost_total', 'currency_id', 'total_in_sar')
     def _compute_total_supplier_cost(self):
         for order in self:
-            if order.currency_id.name == 'USD':  # ✅ تم التعديل من 'US' إلى 'USD'
+            if order.currency_id.name == 'USD':
                 order.total_supplier_cost = order.total_in_sar + order.landed_cost_total
             else:
                 order.total_supplier_cost = order.amount_total + order.landed_cost_total
+
+    @api.depends('amount_total', 'currency_id')
+    def _compute_total_in_sar(self):
+        for order in self:
+            company_currency = order.company_currency_id
+            if order.currency_id != company_currency:
+                order.total_in_sar = order.currency_id._convert(
+                    order.amount_total, company_currency, order.company_id, order.date_order or fields.Date.today())
+            else:
+                order.total_in_sar = order.amount_total
+
     # @api.depends('amount_total', 'landed_cost_total')
     # def _compute_total_supplier_cost(self):
     #     for order in self:
