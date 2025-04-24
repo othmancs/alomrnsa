ad241eae40495e7becd7fa36628bb0867750ef59
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-24 11:11:15 +0300

    Update purchase_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- stock_landed_costs_OCS/models/purchase_order.py
+++ stock_landed_costs_OCS/models/purchase_order.py
@@ -43,12 +43,14 @@ class PurchaseOrder(models.Model):
                     total += sum(landed_costs.mapped('amount_total'))
             order.landed_cost_total = total
 
-    @api.depends('amount_total', 'landed_cost_total', 'currency_id', 'total_in_sar')
+    @api.depends('amount_total', 'landed_cost_total', 'currency_id')
     def _compute_total_supplier_cost(self):
         for order in self:
             company_currency = order.company_currency_id
             if order.currency_id != company_currency:
-                order.total_supplier_cost = order.total_in_sar + order.landed_cost_total
+                amount_in_sar = order.currency_id._convert(
+                    order.amount_total, company_currency, order.company_id, order.date_order or fields.Date.today())
+                order.total_supplier_cost = amount_in_sar + order.landed_cost_total
             else:
                 order.total_supplier_cost = order.amount_total + order.landed_cost_total
 
