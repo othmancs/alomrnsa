e04e33cfa9b9d4b7a14b1e3300de403d9f1bda81
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-24 10:09:28 +0300

    Update purchase_order.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- stock_landed_costs_OCS/models/purchase_order.py
+++ stock_landed_costs_OCS/models/purchase_order.py
@@ -27,8 +27,19 @@ class PurchaseOrder(models.Model):
                     ])
                     total += sum(landed_costs.mapped('amount_total'))
             order.landed_cost_total = total
-    
-    @api.depends('amount_total', 'landed_cost_total')
+    @api.depends('amount_total', 'landed_cost_total', 'currency_id')
     def _compute_total_supplier_cost(self):
         for order in self:
-            order.total_supplier_cost = order.amount_total + order.landed_cost_total
+            if order.currency_id.name == 'US':
+                # إذا كانت العملة دولار أمريكي، نجمع landed_cost_total مع total_in_sar
+                order.total_supplier_cost = order.total_in_sar+ order.landed_cost_total
+            elif order.currency_id.name == 'SAR':
+    
+               order.total_supplier_cost = order.amount_total + order.landed_cost_total        
+            else:
+                # حالة افتراضية إذا كانت العملة غير المذكورة
+                order.total_supplier_cost = order.landed_cost_total
+    # @api.depends('amount_total', 'landed_cost_total')
+    # def _compute_total_supplier_cost(self):
+    #     for order in self:
+    #         order.total_supplier_cost = order. + order.landed_cost_total
