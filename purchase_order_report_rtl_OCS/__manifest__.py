e748ded49083f0f2d4059bc313c61a2a67c1ac7e
othmancs <61784613+othmancs@users.noreply.github.com>
2025-04-26 11:48:24 +0300

    Add files via upload
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

---
+++ purchase_order_report_rtl_OCS/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Purchase Order Report RTL Custom',
+    'version': '1.0',
+    'summary': 'تعديلات على طباعة أمر الشراء: عربي، RTL، معلومات إضافية',
+    'author': 'OTHMANCS',
+    'depends': ['purchase'],
+    'data': [
+        'views/purchase_order_report.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
