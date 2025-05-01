f0f29eda599379eedcc533b9b10a0841c8e25789
othmancs <61784613+othmancs@users.noreply.github.com>
2025-05-01 08:47:35 +0300

    Update stock_move_line.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- restrict_qty_done/stock_move_line.py
+++ restrict_qty_done/stock_move_line.py
@@ -1,5 +1,5 @@
 from odoo.exceptions import ValidationError
-from odoo import models, api
+from odoo import models, api, fields
 
 class StockMove(models.Model):
     _inherit = 'stock.move'
@@ -7,6 +7,13 @@ class StockMove(models.Model):
     @api.constrains('quantity_done')
     def _check_quantity_done(self):
         for record in self:
-            if record.picking_id.picking_type_id.code == "outgoing":  # تحقق من أن العملية "خروج"
-                if record.quantity_done > record.product_uom_qty:  # الكمية المنفذة لا تتجاوز الكمية المطلوبة
-                    raise ValidationError("⚠️ الكمية المنفذة لا يمكن أن تكون أكبر من الكمية المطلوبة في عملية النقل!")
+            if record.picking_id.picking_type_id.code == "outgoing":
+                # استخدام دالة تقريب لتجنب مشاكل الفاصلة العائمة
+                quantity_done = fields.Float.round(record.quantity_done, record.product_uom.rounding)
+                product_uom_qty = fields.Float.round(record.product_uom_qty, record.product_uom.rounding)
+                
+                if quantity_done > product_uom_qty:
+                    raise ValidationError(
+                        "⚠️ الكمية المنفذة (%s) لا يمكن أن تكون أكبر من الكمية المطلوبة (%s) في عملية النقل!" %
+                        (quantity_done, product_uom_qty)
+                    )
