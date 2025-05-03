3ba61e74eeed35084d1e870aa43ba657cceba65b
othmancs <61784613+othmancs@users.noreply.github.com>
2025-05-04 00:36:34 +0300

    Update account_move.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- account_discount_module_OCS/models/account_move.py
+++ account_discount_module_OCS/models/account_move.py
@@ -14,19 +14,25 @@ class AccountMove(models.Model):
     @api.constrains('allowed_discount')
     def _check_allowed_discount(self):
         for move in self:
-            if move.allowed_discount < 0 or move.allowed_discount > 1:
+            if move.move_type == 'out_invoice' and (move.allowed_discount < 0 or move.allowed_discount > 1):
                 raise ValidationError(_('الخصم المسموح به يجب أن يكون بين 0 و 1 ريال فقط!'))
     
-    def _recompute_tax_lines(self, recompute_tax_base_amount=False):
-        res = super(AccountMove, self)._recompute_tax_lines(recompute_tax_base_amount)
+    @api.depends('invoice_line_ids', 'allowed_discount', 'tax_totals')
+    def _compute_amount(self):
+        super()._compute_amount()
         for move in self:
-            if move.allowed_discount:
-                # حساب الخصم مع التأكد من عدم تجاوز 1 ريال
+            if move.move_type == 'out_invoice' and move.allowed_discount:
                 discount_amount = min(move.allowed_discount, 1)
-                # تحديث إجمالي الضرائب مع الخصم
+                # تحديث الإجماليات
+                move.amount_total = move.amount_total - discount_amount
                 if move.tax_totals:
-                    move.tax_totals['amount_total'] -= discount_amount
-                    move.tax_totals['amount_untaxed'] -= discount_amount
-                # تحديث إجمالي الفاتورة
-                move.amount_total = move.amount_untaxed + move.amount_tax - discount_amount
+                    move.tax_totals = {
+                        **move.tax_totals,
+                        'amount_total': move.tax_totals['amount_total'] - discount_amount,
+                        'formatted_amount_total': move.currency_id.format(move.tax_totals['amount_total'] - discount_amount)
+                    }
+    
+    def _recompute_tax_lines(self, recompute_tax_base_amount=False):
+        res = super()._recompute_tax_lines(recompute_tax_base_amount)
+        self._compute_amount()
         return res
