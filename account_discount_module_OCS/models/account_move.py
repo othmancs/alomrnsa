70eb0ef740a44acce5cd90af428511ce321cda49
othmancs <61784613+othmancs@users.noreply.github.com>
2025-05-04 00:47:59 +0300

    Update account_move.py
    Signed-off-by: othmancs <61784613+othmancs@users.noreply.github.com>

--- account_discount_module_OCS/models/account_move.py
+++ account_discount_module_OCS/models/account_move.py
@@ -17,23 +17,19 @@ class AccountMove(models.Model):
             if move.move_type == 'out_invoice' and (move.allowed_discount < 0 or move.allowed_discount > 1):
                 raise ValidationError(_('الخصم المسموح به يجب أن يكون بين 0 و 1 ريال فقط!'))
     
-    def _recompute_dynamic_lines(self, recompute_all_taxes=False, recompute_tax_base_amount=False):
-        res = super(AccountMove, self)._recompute_dynamic_lines(
-            recompute_all_taxes=recompute_all_taxes,
-            recompute_tax_base_amount=recompute_tax_base_amount
-        )
-        
+    @api.depends('invoice_line_ids', 'allowed_discount')
+    def _compute_amount(self):
+        super()._compute_amount()
         for move in self:
             if move.move_type == 'out_invoice' and move.allowed_discount:
                 discount_amount = min(move.allowed_discount, 1)
-                move.amount_total = move.amount_untaxed + move.amount_tax - discount_amount
+                move.amount_total -= discount_amount
                 
-                if hasattr(move, 'tax_totals') and move.tax_totals:
+                if move.tax_totals:
                     move.tax_totals['amount_total'] = move.amount_total
                     move.tax_totals['formatted_amount_total'] = move.currency_id.format(move.amount_total)
-        return res
     
     @api.onchange('allowed_discount')
     def _onchange_allowed_discount(self):
         if self.move_type == 'out_invoice':
-            self._recompute_dynamic_lines()
+            self._compute_amount()
