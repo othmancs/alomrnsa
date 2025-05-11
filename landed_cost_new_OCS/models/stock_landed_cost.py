f5509a79e613eb523713dc7021fa2f15cffe889a
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-11 12:32:30 +0300

    Update stock_landed_cost.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- landed_cost_new_OCS/models/stock_landed_cost.py
+++ landed_cost_new_OCS/models/stock_landed_cost.py
@@ -15,35 +15,42 @@ class LandedCostLine(models.Model):
         super(LandedCostLine, self)._compute_cost_lines()
         for line in self:
             if line.split_method == 'construction_costs':
-                total_costs = line.cost_id.amount_total
+                total_costs = line.cost_id.amount_total  # إجمالي التكاليف (18,613.83 في المثال)
                 pickings = line.cost_id.picking_ids
                 
                 if pickings:
-                    # حساب إجمالي تكلفة الشراء لجميع المنتجات
+                    # حساب إجمالي تكلفة الشراء لجميع المنتجات (82,761.57 في المثال)
                     all_moves = pickings.mapped('move_lines')
                     purchase_lines = all_moves.mapped('purchase_line_id')
                     
                     if purchase_lines:
                         total_purchase_cost = sum(
-                            pl.price_unit * pl.product_qty 
+                            pl.price_unit * pl.product_qty * (pl.currency_id.rate if pl.currency_id != pl.company_id.currency_id else 1)
                             for pl in purchase_lines
                             if pl.price_unit and pl.product_qty
                         )
                         
                         if total_purchase_cost != 0:
-                            # حساب النسبة المئوية للتكاليف من إجمالي الشراء
+                            # حساب النسبة المئوية (22% في المثال)
                             percentage = total_costs / total_purchase_cost
                             
                             # الحصول على حركة المخزن المرتبطة بالخط الحالي
                             current_move = line.move_id
                             if current_move and current_move.purchase_line_id:
-                                # سعر الشراء للصنف الحالي
-                                product_price = current_move.purchase_line_id.price_unit
+                                purchase_line = current_move.purchase_line_id
+                                
+                                # تحويل سعر الشراء للعملة الأساسية إذا كان مختلفاً
+                                product_price = purchase_line.price_unit
+                                if purchase_line.currency_id != purchase_line.company_id.currency_id:
+                                    product_price = purchase_line.price_unit * purchase_line.currency_id.rate
+                                
                                 # كمية الصنف الحالي
                                 product_qty = current_move.product_qty or 1
                                 
-                                # حساب التكلفة الجديدة (النسبة المئوية × إجمالي تكلفة الشراء للمنتج)
-                                line.price_unit = float_round(
-                                    (percentage * (product_price * product_qty)),
+                                # حساب التكلفة الجديدة (النسبة × سعر الوحدة بعد التحويل)
+                                cost_per_unit = float_round(
+                                    (percentage * product_price),
                                     precision_digits=2
                                 )
+                                
+                                line.price_unit = cost_per_unit
