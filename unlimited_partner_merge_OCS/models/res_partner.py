1e7231f7a82d17120f277ea5f9daeb2a5c84d303
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-11 22:51:12 +0300

    Update res_partner.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- unlimited_partner_merge_OCS/models/res_partner.py
+++ unlimited_partner_merge_OCS/models/res_partner.py
@@ -1,16 +1,9 @@
 from odoo import models, api, _
 
-class PartnerMergeAutomatic(models.TransientModel):
+class PartnerMerge(models.TransientModel):
     _inherit = 'base.partner.merge.automatic.wizard'
 
     def _merge(self, partner_ids, dst_partner=None, extra_checks=True):
-        """ Override merge function to remove 3-partner limit and handle new parameters """
         if len(partner_ids) < 2:
-            raise ValidationError(_("يجب اختيار شريكين على الأقل للدمج."))
-        
-        # استدعاء الدالة الأصلية مع جميع المعاملات المطلوبة
-        return super(PartnerMergeAutomatic, self)._merge(
-            partner_ids,
-            dst_partner=dst_partner,
-            extra_checks=extra_checks
-        )
+            raise ValidationError(_("Please select at least 2 partners to merge."))
+        return super()._merge(partner_ids, dst_partner, extra_checks)
