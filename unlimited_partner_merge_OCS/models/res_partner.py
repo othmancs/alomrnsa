f898b16c854efee7cb525f3a524c669ab9b1011c
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-11 22:55:51 +0300

    Update res_partner.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- unlimited_partner_merge_OCS/models/res_partner.py
+++ unlimited_partner_merge_OCS/models/res_partner.py
@@ -4,13 +4,25 @@ from odoo.exceptions import ValidationError
 class PartnerMerge(models.TransientModel):
     _inherit = 'base.partner.merge.automatic.wizard'
 
-    def _merge(self, partner_ids, dst_partner=None, extra_checks=True):
+    @api.model
+    def _get_merge_action(self, partner_ids):
+        """تجاوز كامل لإزالة القيود"""
         if len(partner_ids) < 2:
-            raise ValidationError(_("يجب اختيار شريكين على الأقل للدمج."))
-        return super()._merge(partner_ids, dst_partner, extra_checks)
+            raise ValidationError(_("يجب اختيار شريكين على الأقل"))
+        return {
+            'name': _('Merge Partners'),
+            'view_mode': 'form',
+            'res_model': 'base.partner.merge.automatic.wizard',
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+            'context': {
+                'default_partner_ids': [(6, 0, partner_ids)],
+                'default_max_merge': float('inf')  # إزالة الحد الأقصى
+            }
+        }
 
     def action_merge(self):
-        self.ensure_one()
+        """تجاوز دالة الدمج الرئيسية"""
         if len(self.partner_ids) < 2:
-            raise ValidationError(_("يجب اختيار شريكين على الأقل للدمج."))
+            raise ValidationError(_("يجب اختيار شريكين على الأقل"))
         return super().action_merge()
