37c38295c91f9ca6ad9648b07fd1fe2e82313bca
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-15 09:15:43 +0300

    Update sale_order.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- sb_seller_field/model/sale_order.py
+++ sb_seller_field/model/sale_order.py
@@ -4,5 +4,10 @@ from odoo import fields, models
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    created_by_id = fields.Many2one('res.partner', string='انشأ من قبل', domain="[('branch_id', '=', branch_id)]", required=True)
-
+    created_by_id = fields.Many2one(
+        'res.partner', 
+        string='انشأ من قبل', 
+        domain="[('branch_id', '=', branch_id)]", 
+        required=True,
+        readonly="[('state', '!=', 'draft')]"  # سيكون الحقل للقراءة فقط عندما لا تكون الحالة مسودة
+    )
