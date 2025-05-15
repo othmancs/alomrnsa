28114d7b84e7295b48667527737fe9b24649c313
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-15 10:35:17 +0300

    Update sales_report_wizard.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- sr_sales_report_saleperson_groupby_OCS/wizards/sales_report_wizard.py
+++ sr_sales_report_saleperson_groupby_OCS/wizards/sales_report_wizard.py
@@ -2,33 +2,47 @@ from odoo import models, fields, api, _
 
 class SalesReportBySalesperson(models.TransientModel):
     _name = 'sale.salesperson.report'
-    _description = 'Sales Report by Salesperson'
+    _description = 'Sales Report by Creator (created_by_id)'
 
     start_date = fields.Datetime(string="Start Date", required=True)
     end_date = fields.Datetime(string="End Date", required=True)
-    salesperson_ids = fields.Many2many('res.users', string="Salesperson", required=True)
+    salesperson_ids = fields.Many2many(
+        'res.users', 
+        string="Salesperson (Creator)", 
+        required=True,
+        # اختيار المستخدمين الذين لديهم أوامر مبيعات كمُنشئين (created_by_id)
+        domain=lambda self: [('id', 'in', self._get_available_creators().ids)]
+    )
+
+    def _get_available_creators(self):
+        """ترجع قائمة بالمستخدمين الذين قاموا بإنشاء أوامر مبيعات"""
+        sale_orders = self.env['sale.order'].search([])
+        creator_ids = sale_orders.mapped('created_by_id').ids
+        return self.env['res.users'].browse(creator_ids)
 
     def print_sale_report_by_salesperson(self):
-        sales_order = self.env['sale.order'].search([])
+        sales_orders = self.env['sale.order'].search([
+            ('date_order', '>=', self.start_date),
+            ('date_order', '<=', self.end_date),
+            ('created_by_id', 'in', self.salesperson_ids.ids)
+        ])
+
         sale_order_groupby_dict = {}
         for salesperson in self.salesperson_ids:
-            # تغيير من x.user_id إلى x.created_by_id
-            filtered_sale_order = list(filter(lambda x: x.created_by_id == salesperson, sales_order))
-            filtered_by_date = list(
-                filter(lambda x: self.start_date <= x.date_order <= self.end_date, filtered_sale_order))
-            sale_order_groupby_dict[salesperson.name] = filtered_by_date
+            # تصفية الأوامر حسب created_by_id
+            filtered_orders = sales_orders.filtered(lambda o: o.created_by_id == salesperson)
+            sale_order_groupby_dict[salesperson.name] = filtered_orders
 
         final_dist = {}
-        for salesperson in sale_order_groupby_dict.keys():
+        for salesperson, orders in sale_order_groupby_dict.items():
             sale_data = []
-            for order in sale_order_groupby_dict[salesperson]:
-                temp_data = [
+            for order in orders:
+                sale_data.append([
                     order.name,
                     order.date_order,
                     order.partner_id.name,
                     order.amount_total,
-                ]
-                sale_data.append(temp_data)
+                ])
             final_dist[salesperson] = sale_data
 
         datas = {
@@ -36,7 +50,7 @@ class SalesReportBySalesperson(models.TransientModel):
             'model': 'sale.salesperson.report',
             'form': final_dist,
             'start_date': self.start_date,
-            'end_date': self.end_date
+            'end_date': self.end_date,
         }
 
-        return self.env.ref('sr_sales_report_saleperson_groupby.action_report_by_salesperson').report_action(self,data=datas)
+        return self.env.ref('sr_sales_report_saleperson_groupby.action_report_by_salesperson').report_action(self, data=datas)
