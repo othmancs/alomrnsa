4bc2c9bed888388ef83c89f4fa04e441ce720ec6
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-17 11:59:33 +0300

    Update hooks.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- auto_employee_contact/hooks.py
+++ auto_employee_contact/hooks.py
@@ -3,24 +3,17 @@
 from odoo import api, SUPERUSER_ID
 
 def create_contacts_for_existing_employees(cr, registry):
-    """
-    Post-init hook لإنشاء جهات اتصال للموظفين الموجودين
-    """
+    """Create contacts for all existing employees"""
     env = api.Environment(cr, SUPERUSER_ID, {})
-    Employee = env['hr.employee']
+    employees = env['hr.employee'].search([('partner_id', '=', False)])
     
-    employees_without_partner = Employee.search([('partner_id', '=', False)])
-    
-    if employees_without_partner:
-        # إنشاء جهات اتصال للموظفين الموجودين
-        for employee in employees_without_partner:
-            partner = env['res.partner'].create({
-                'name': employee.name,
-                'email': employee.work_email,
-                'phone': employee.work_phone,
-                'mobile': employee.mobile_phone,
-                'company_id': employee.company_id.id,
-                'type': 'contact',
-                'active': True,
-            })
-            employee.write({'partner_id': partner.id})
+    for emp in employees:
+        partner = env['res.partner'].create({
+            'name': emp.name,
+            'email': emp.work_email,
+            'phone': emp.work_phone,
+            'mobile': emp.mobile_phone,
+            'company_id': emp.company_id.id,
+            'type': 'contact'
+        })
+        emp.write({'partner_id': partner.id})
