b382fa06cb0932c21cf0fdc7a136640a7db76322
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-24 11:58:37 +0300

    Update stock_landed_cost.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- landed_cost_new_OCS/models/stock_landed_cost.py
+++ landed_cost_new_OCS/models/stock_landed_cost.py
@@ -1,65 +1,48 @@
-from odoo import models, fields, api
-from odoo.tools.float_utils import float_round
-from collections import defaultdict
-
-class LandedCostLine(models.Model):
-    _inherit = 'stock.landed.cost.lines'
-
-    percentage = fields.Float(
-        string="النسبة المئوية", 
-        compute='_compute_percentage', 
-        store=True, 
-        digits=(16, 2)
-    )
-
-    split_method = fields.Selection(
-        selection_add=[('construction_costs', 'تكاليف العمران')],
-        ondelete={'construction_costs': 'set default'},
-        default='by_quantity'
-    )
-
-    @api.depends('cost_id.amount_total', 'cost_id.picking_ids')
-    def _compute_percentage(self):
-        for line in self:
-            if line.split_method == 'construction_costs':
-                total_costs = line.cost_id.amount_total
-                pickings = line.cost_id.picking_ids
-
-                if pickings:
-                    # حساب إجمالي تكلفة الشراء بالريال (total_in_sar)
-                    total_purchase_cost = sum(
-                        move.purchase_line_id.price_unit * move.purchase_line_id.product_qty 
-                        * (move.purchase_line_id.currency_id.rate if move.purchase_line_id.currency_id != move.purchase_line_id.company_id.currency_id else 1)
-                        for move in pickings.move_ids
-                        if move.purchase_line_id and move.purchase_line_id.price_unit and move.purchase_line_id.product_qty
-                    )
-
-                    # حساب النسبة المئوية (إجمالي التكاليف / إجمالي تكلفة الشراء)
-                    line.percentage = (total_costs / total_purchase_cost) * 100 if total_purchase_cost != 0 else 0.0
-                else:
-                    line.percentage = 0.0
-            else:
-                line.percentage = 0.0
-
-    @api.depends('price_unit', 'split_method', 'cost_id', 'percentage')
-    def _compute_cost_lines(self):
-        super(LandedCostLine, self)._compute_cost_lines()
-
-        # معالجة خطوط تكاليف العمران فقط
-        for line in self.filtered(lambda l: l.split_method == 'construction_costs' and l.move_id):
-            move = line.move_id
-            if move and move.purchase_line_id:
-                purchase_line = move.purchase_line_id
-                
-                # حساب تكلفة شراء الحبة المنتج بالريال
-                product_price = purchase_line.price_unit
-                if purchase_line.currency_id != purchase_line.company_id.currency_id:
-                    product_price *= purchase_line.currency_id.rate
-                
-                # حساب التكلفة الإضافية للمنتج (النسبة المئوية * تكلفة شراء الحبة)
-                cost_per_unit = float_round(
-                    (line.percentage / 100) * product_price,
-                    precision_digits=2
-                )
-                
-                line.price_unit = cost_per_unit
+<?xml version="1.0" encoding="utf-8"?>
+<odoo>
+    <!-- إضافة خيار تقسيم تكاليف العمران إلى واجهة المستخدم -->
+    <record id="view_stock_landed_cost_lines_form_inherit" model="ir.ui.view">
+        <field name="name">stock.landed.cost.lines.form.inherit</field>
+        <field name="model">stock.landed.cost.lines</field>
+        <field name="inherit_id" ref="stock_landed_costs.view_stock_landed_cost_lines_form"/>
+        <field name="arch" type="xml">
+            <!-- تعديل حقل طريقة التقسيم لإضافة خيار تكاليف العمران -->
+            <field name="split_method" position="attributes">
+                <attribute name="options">{
+                    "no_open": True,
+                    "no_create": True,
+                    "selection_add": [
+                        ["construction_costs", "تكاليف العمران"]
+                    ]
+                }</attribute>
+            </field>
+            
+            <!-- إظهار حقل النسبة المئوية فقط عند اختيار تكاليف العمران -->
+            <field name="percentage" position="attributes">
+                <attribute name="attrs">{'invisible': [('split_method', '!=', 'construction_costs')]}</attribute>
+                <attribute name="readonly">1</attribute>
+            </field>
+            
+            <!-- إضافة تعليمات للمستخدم -->
+            <xpath expr="//field[@name='split_method']" position="after">
+                <div class="alert alert-info" attrs="{'invisible': [('split_method', '!=', 'construction_costs')]}">
+                    <strong>طريقة حساب تكاليف العمران:</strong><br/>
+                    1. يتم حساب النسبة المئوية (إجمالي التكاليف / إجمالي تكلفة الشراء بالريال)<br/>
+                    2. يتم تطبيق النسبة على تكلفة كل منتج
+                </div>
+            </xpath>
+        </field>
+    </record>
+    
+    <!-- تعديل عرض قائمة خطوط التكاليف لإظهار النسبة المئوية -->
+    <record id="view_stock_landed_cost_lines_tree_inherit" model="ir.ui.view">
+        <field name="name">stock.landed.cost.lines.tree.inherit</field>
+        <field name="model">stock.landed.cost.lines</field>
+        <field name="inherit_id" ref="stock_landed_costs.view_stock_landed_cost_lines_tree"/>
+        <field name="arch" type="xml">
+            <field name="split_method" position="after">
+                <field name="percentage" attrs="{'invisible': [('split_method', '!=', 'construction_costs')]}"/>
+            </field>
+        </field>
+    </record>
+</odoo>
