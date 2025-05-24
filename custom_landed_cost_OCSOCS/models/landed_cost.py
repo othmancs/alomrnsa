0a0256bcde131e8889634356d3a0c9e03cca0318
Othman Almashaqba <61784613+othmancs@users.noreply.github.com>
2025-05-24 09:45:00 +0300

    Update landed_cost.py
    Signed-off-by: Othman Almashaqba <61784613+othmancs@users.noreply.github.com>

--- custom_landed_cost_OCSOCS/models/landed_cost.py
+++ custom_landed_cost_OCSOCS/models/landed_cost.py
@@ -7,8 +7,24 @@ class LandedCost(models.Model):
     def _get_cost_distribution_methods(self):
         """ إضافة خيار تكاليف العمران إلى طرق التوزيع """
         methods = super(LandedCost, self)._get_cost_distribution_methods()
-        methods.append(('urbanization', 'تكاليف العمران (حسب القيمة)'))
+        methods.append(('construction_costs', 'تكاليف العمران (حسب القيمة)'))
         return methods
+
+    def _create_valuation_lines(self):
+        """ تعديل الدالة لتجميع بنود المنتجات """
+        lines = super(LandedCost, self)._create_valuation_lines()
+        grouped_lines = {}
+        
+        # تجميع البنود حسب المنتج
+        for line in lines:
+            key = (line['product_id'], line['cost_line_id'])
+            if key not in grouped_lines:
+                grouped_lines[key] = line.copy()
+            else:
+                grouped_lines[key]['additional_landed_cost'] += line['additional_landed_cost']
+                grouped_lines[key]['quantity'] += line['quantity']
+        
+        return list(grouped_lines.values())
 
 class LandedCostLines(models.Model):
     _inherit = 'stock.landed.cost.lines'
@@ -18,7 +34,7 @@ class LandedCostLines(models.Model):
         """ تخصيص طريقة الحساب لخيار تكاليف العمران """
         super(LandedCostLines, self)._compute_price_unit()
         for line in self:
-            if line.split_method == 'urbanization':
+            if line.split_method == 'construction_costs':
                 if not line.cost_id:
                     line.price_unit = 0.0
                     continue
@@ -27,7 +43,4 @@ class LandedCostLines(models.Model):
                 for line_inv in line.cost_id.picking_ids.move_ids:
                     total += line_inv.product_id.standard_price * line_inv.product_qty
                 
-                if total == 0.0:
-                    line.price_unit = 0.0
-                else:
-                    line.price_unit = line.price_unit / total
+                line.price_unit = line.price_unit / total if total else 0.0
